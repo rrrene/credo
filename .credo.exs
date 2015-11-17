c88d6019fdde33f801c220c91d0c8f80da4825aa
René Föhring <rf@bamaru.de>
2015-11-17 19:26:43 +0100

    Update .credo.exs with more details

--- .credo.exs
+++ .credo.exs
@@ -1,13 +1,14 @@
 # This file contains the configuration for Credo.
+#
 # If you find anything wrong or unclear in this file, please report an
 # issue on GitHub: https://github.com/rrrene/credo/issues
 %{
-  # You can have as many configs as you like under the "configs" key.
+  # You can have as many configs as you like in the `configs:` field.
   configs: [
     %{
       #
-      # names are arbitrary, default is "default"
-      # run any config using the "-C" switch
+      # Run any config using `mix credo -C <name>`. If no config name is given
+      # "default" is used.
       name: "default",
       #
       # these are the files included in the analysis
@@ -19,25 +20,30 @@
         excluded: []
       },
       #
-      # all these checks are run
-      # you can include configuration as the second element of the tuple
+      # The `checks:` field contains all the checks that are run. You can
+      # customize the parameters of any given check by adding a second element
+      # to the tuple.
       #
-      # there are two ways of deactivating a check:
+      # There are two ways of deactivating a check:
       # 1. deleting the check from this list
-      # 2. putting `false` as second element (to quickly "comment it out")
+      # 2. putting `false` as second element (to quickly "comment it out").
       #
       checks: [
         {Credo.Check.Consistency.ExceptionNames},
         {Credo.Check.Consistency.LineEndings},
         {Credo.Check.Consistency.TabsOrSpaces},
 
-        {Credo.Check.Design.AliasUsage},
-        {Credo.Check.Design.DuplicatedCode},
+        # For some checks, like AliasUsage, you can only customize the priority
+        # Priority values are: `low, normal, high, higher`
+        {Credo.Check.Design.AliasUsage, priority: :low},
+        # For others you can set parameters
+        {Credo.Check.Design.DuplicatedCode, mass_threshold: 16, nodes_threshold: 2},
+
         {Credo.Check.Design.TagFIXME},
         {Credo.Check.Design.TagTODO},
 
         {Credo.Check.Readability.FunctionNames},
-        {Credo.Check.Readability.MaxLineLength},
+        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 80},
         {Credo.Check.Readability.ModuleAttributeNames},
         {Credo.Check.Readability.ModuleNames},
         {Credo.Check.Readability.PredicateFunctionNames},
