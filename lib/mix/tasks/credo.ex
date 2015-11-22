645ecc0916d53fc14d9199d4832e3db569613fb0
René Föhring <rf@bamaru.de>
2015-11-22 17:00:39 +0100

    Update mix task description

--- lib/mix/tasks/credo.ex
+++ lib/mix/tasks/credo.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Credo do
   use Mix.Task
 
-  @shortdoc  "Run code analysis"
+  @shortdoc  "Run code analysis (use `--help` for options)"
   @moduledoc @shortdoc
 
   def run(argv) do
