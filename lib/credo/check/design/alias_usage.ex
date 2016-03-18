b9bf01305fe3d41bd55cf22809b1a9aac32f59d4
René Föhring <rf@bamaru.de>
2016-03-18 18:35:37 +0100

    Fix false positives for AliasUsage
    Fixes #35

--- lib/credo/check/design/alias_usage.ex
+++ lib/credo/check/design/alias_usage.ex
@@ -61,17 +61,38 @@ defmodule Credo.Check.Design.AliasUsage do
     Credo.Code.traverse(ast, &traverse(&1, &2, issue_meta, excluded_namespaces, excluded_lastnames))
   end
 
-  # Ignore multi alias call
-  defp traverse({:., _, [{:__aliases__, meta, mod_list}, :{}]} = ast, issues, _source_file, _excluded_namespaces, _excluded_lastnames) do
+  defp traverse({:defmodule, _, _} = ast, issues, issue_meta, excluded_namespaces, excluded_lastnames) do
+    aliases = Credo.Code.traverse(ast, &find_aliases/2)
+    new_issues = Credo.Code.traverse(ast, &find_alias_usage(&1, &2, issue_meta, excluded_namespaces, excluded_lastnames, aliases))
+    {ast, issues ++ new_issues}
+  end
+  defp traverse(ast, issues, _source_file, _excluded_namespaces, _excluded_lastnames) do
     {ast, issues}
   end
-  defp traverse({:., _, [{:__aliases__, meta, mod_list}, fun_atom]} = ast, issues, issue_meta, excluded_namespaces, excluded_lastnames) when is_list(mod_list) and is_atom(fun_atom) do
+
+  # Ignore multi alias call
+  defp find_alias_usage({:., _, [{:__aliases__, _, _}, :{}]} = ast, issues, _issue_meta, _excluded_namespaces, _excluded_lastnames, _aliases) do
+    {ast, issues}
+  end
+  defp find_alias_usage({:., _, [{:__aliases__, meta, mod_list}, fun_atom]} = ast, issues, issue_meta, excluded_namespaces, excluded_lastnames, aliases) when is_list(mod_list) and is_atom(fun_atom) do
     if Enum.count(mod_list) > 1 && !Enum.any?(mod_list, &tuple?/1) do
       first_name = mod_list |> List.first |> to_string
       last_name = mod_list |> List.last |> to_string
-      if !Enum.member?(excluded_namespaces, first_name) && !Enum.member?(excluded_lastnames, last_name) do
-        trigger = mod_list |> Enum.join(".")
-        {ast, issues ++ [issue_for(issue_meta, meta[:line], trigger)]}
+      excluded? =
+        Enum.member?(excluded_namespaces, first_name) ||
+        Enum.member?(excluded_lastnames, last_name)
+
+      if !excluded? do
+        conflicting_alias =
+          aliases
+          |> Enum.find(&conflicting_alias?(&1, mod_list))
+
+        if conflicting_alias do
+          {ast, issues}
+        else
+          trigger = mod_list |> Enum.join(".")
+          {ast, issues ++ [issue_for(issue_meta, meta[:line], trigger)]}
+        end
       else
         {ast, issues}
       end
@@ -79,12 +100,48 @@ defmodule Credo.Check.Design.AliasUsage do
       {ast, issues}
     end
   end
-  defp traverse(ast, issues, _source_file, _excluded_namespaces, _excluded_lastnames) do
+  defp find_alias_usage(ast, issues, _source_file, _excluded_namespaces, _excluded_lastnames, _aliases) do
     {ast, issues}
+  end
+
+  # Returns true if mod_list and alias_name would result in the same alias
+  # since they share the same last name.
+  defp conflicting_alias?(alias_name, mod_list) do
+    last_name = mod_list |> List.last |> to_string
+    full_name = mod_list |> to_module_name
+    alias_last_name = alias_name |> String.split(".") |> List.last
+
+    full_name != alias_name && alias_last_name == last_name
+  end
+
+  # Single alias
+  defp find_aliases({:alias, _, [{:__aliases__, _, mod_list}]} = ast, aliases) do
+    module_names = mod_list |> to_module_name |> List.wrap
+    {ast, aliases ++ module_names}
+  end
+  # Multi alias
+  defp find_aliases({:alias, _, [{{:., _, [{:__aliases__, _, mod_list}, :{}]}, _, multi_mod_list}]} = ast, aliases) do
+    module_names =
+      multi_mod_list
+      |> Enum.map(fn(tuple) ->
+          [to_module_name(mod_list), to_module_name(tuple)] |> to_module_name
+        end)
+
+    {ast, aliases ++ module_names}
+  end
+  defp find_aliases(ast, aliases) do
+    {ast, aliases}
   end
 
   def tuple?(t) when is_tuple(t), do: true
   def tuple?(_), do: false
+
+  defp to_module_name({:__aliases__, _, mod_list}) do
+    mod_list |> to_module_name()
+  end
+  defp to_module_name(mod_list) when is_list(mod_list) do
+    mod_list |> Enum.join(".")
+  end
 
 
   defp issue_for(issue_meta, line_no, trigger) do
