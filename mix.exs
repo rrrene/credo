14b3130171fbb6fdfb398ba234e8d8f5eac578a3
René Föhring <rf@bamaru.de>
2016-05-22 19:14:41 +0200

    Bump version to 0.4.0-beta2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Credo.Mixfile do
   def project do
     [
       app: :credo,
-      version: "0.4.0-beta1",
+      version: "0.4.0-beta2",
       elixir: "~> 1.1",
       escript: [main_module: Credo.CLI],
       build_embedded: Mix.env == :prod,
