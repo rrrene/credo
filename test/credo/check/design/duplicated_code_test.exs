1a21a9a65f99b90c163d75cbe8f8d656d9abee7d
v.tikhonov <vladimir@tikhonov.by>
2016-05-28 22:15:15 +0300

    Ignore metadata in OperationOnSameValues and BoolOperationOnSameValues checks

--- test/credo/check/design/duplicated_code_test.exs
+++ test/credo/check/design/duplicated_code_test.exs
@@ -141,55 +141,6 @@ end
   end
 
 
-  test "returns ast without metadata" do
-    ast =
-      {:__block__, [],
- [{:defmodule, [line: 1],
-   [{:__aliases__, [counter: 0, line: 1], [:M1]},
-    [do: {:def, [line: 2],
-      [{:when, [line: 2],
-        [{:myfun, [line: 2], [{:p1, [line: 2], nil}, {:p2, [line: 2], nil}]},
-         {:is_list, [line: 2], [{:p2, [line: 2], nil}]}]},
-       [do: {:if, [line: 3],
-         [{:==, [line: 3], [{:p1, [line: 3], nil}, {:p2, [line: 3], nil}]},
-          [do: {:p1, [line: 4], nil},
-           else: {:+, [line: 6],
-            [{:p2, [line: 6], nil}, {:p1, [line: 6], nil}]}]]}]]}]]},
-  {:defmodule, [line: 11],
-   [{:__aliases__, [counter: 0, line: 11], [:M2]},
-    [do: {:def, [line: 12],
-      [{:myfun2, [line: 12], [{:p1, [line: 12], nil}, {:p2, [line: 12], nil}]},
-       [do: {:if, [line: 13],
-         [{:==, [line: 13], [{:p1, [line: 13], nil}, {:p2, [line: 13], nil}]},
-          [do: {:p1, [line: 14], nil},
-           else: {:+, [line: 16],
-            [{:p2, [line: 16], nil}, {:p1, [line: 16], nil}]}]]}]]}]]}]}
-    expected =
-      {:__block__, [],
-       [{:defmodule, [],
-         [{:__aliases__, [], [:M1]},
-          [do: {:def, [],
-            [{:when, [],
-              [{:myfun, [], [{:p1, [], nil}, {:p2, [], nil}]},
-               {:is_list, [], [{:p2, [], nil}]}]},
-             [do: {:if, [],
-               [{:==, [], [{:p1, [], nil}, {:p2, [], nil}]},
-                [do: {:p1, [], nil},
-                 else: {:+, [],
-                  [{:p2, [], nil}, {:p1, [], nil}]}]]}]]}]]},
-        {:defmodule, [],
-         [{:__aliases__, [], [:M2]},
-          [do: {:def, [],
-            [{:myfun2, [], [{:p1, [], nil}, {:p2, [], nil}]},
-             [do: {:if, [],
-               [{:==, [], [{:p1, [], nil}, {:p2, [], nil}]},
-                [do: {:p1, [], nil},
-                 else: {:+, [],
-                  [{:p2, [], nil}, {:p1, [], nil}]}]]}]]}]]}]}
-    assert expected == DuplicatedCode.remove_metadata(ast)
-  end
-
-
   test "returns correct mass" do
     {:ok, ast} = """
 defmodule M1 do
