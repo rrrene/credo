9c235562016115d31e226b9e9d7a091f24029af4
René Föhring <rf@bamaru.de>
2016-10-29 09:40:52 +0200

    Use Hex.pm API to retrieve package information

--- lib/credo/check_for_updates.ex
+++ lib/credo/check_for_updates.ex
@@ -1,6 +1,12 @@
 defmodule Credo.CheckForUpdates do
   def run() do
-    all_versions = all_hex_versions("credo")
+    "credo"
+    |> fetch_all_hex_versions()
+    |> do_run()
+  end
+
+  defp do_run(nil), do: nil
+  defp do_run(all_versions) do
     current = Credo.version
 
     if should_update?(all_versions, current) do
@@ -13,21 +19,13 @@ defmodule Credo.CheckForUpdates do
         :orange,
         ", please update with `mix deps.update credo`"
       ]
-      |> warn
+      |> warn()
     end
-  end
-
-  def all_hex_versions(package_name) do
-    Hex.start
-    Hex.Registry.open!(Hex.Registry.Server)
-    Hex.Registry.prefetch([package_name])
-
-    Hex.Registry.versions(package_name)
   end
 
   def should_update?(all_versions, current) do
     latest = latest_version(all_versions, current)
-    Hex.Version.compare(current, latest) == :lt
+    Version.compare(current, latest) == :lt
   end
 
   defp latest_version(all_versions, default) do
@@ -48,12 +46,40 @@ defmodule Credo.CheckForUpdates do
     |> List.last
   end
 
-  def pre_version?(version) do
-    {:ok, version} = Hex.Version.parse(version)
+  defp pre_version?(version) do
+    {:ok, version} = Version.parse(version)
     version.pre != []
   end
 
   defp warn(value) do
     IO.puts(:stderr, Bunt.format(value))
+  end
+
+  defp fetch_all_hex_versions(package_name) do
+    case fetch("https://hex.pm/api/packages/#{package_name}") do
+      nil -> nil
+      package_info -> package_info |> hex_versions()
+    end
+  end
+
+  defp fetch(url) do
+    :httpc.request(:get, {String.to_charlist(url),
+          [{'User-Agent', user_agent()},
+           {'Accept', 'application/vnd.hex+erlang'}]}, [], [])
+    |> convert_response_body()
+  end
+
+  defp user_agent do
+    'Credo/#{Credo.version} (Elixir/#{System.version}) (OTP/#{System.otp_release})'
+  end
+
+  defp convert_response_body({:ok, {_status, _headers, body}}) do
+    body |> IO.iodata_to_binary() |> :erlang.binary_to_term()
+  end
+  defp convert_response_body(_), do: nil
+
+  defp hex_versions(nil), do: nil
+  defp hex_versions(%{"releases" => releases}) do
+    releases |> Enum.map(&(&1["version"]))
   end
 end
