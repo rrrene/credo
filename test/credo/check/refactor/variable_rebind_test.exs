9220e6f79b87f719394f619a3061b45a2084309d
Moritz Schmale <narrow.m@gmail.com>
2016-11-29 21:37:34 +0100

    Implemented #247

---
+++ test/credo/check/refactor/variable_rebind_test.exs
@@ -0,0 +1,44 @@
+defmodule Credo.Check.Refactor.VariableRebindingTest do
+  use Credo.TestHelper
+
+  @described_check Credo.Check.Refactor.VariableRebinding
+
+  test "it should NOT report expected code" do
+"""
+defmodule CredoSampleModule do
+  def some_function(parameter1, parameter2) do
+    a = 1
+    b = 2
+  end
+end
+""" |> to_source_file
+    |> refute_issues(@described_check)
+  end
+
+  test "it should report a violation" do
+"""
+defmodule CredoSampleModule do
+  def some_function(parameter1, parameter2) do
+    a = 1
+    a = 2
+  end
+end
+""" |> to_source_file
+    |> assert_issue(@described_check)
+  end
+
+  test "it should report two violations" do
+"""
+defmodule CredoSampleModule do
+  def some_function() do
+    var_1 = 1 + 3
+    var_b = var1 + 7
+    var_1 = 34
+    var_c = 2456
+    var_b = 2
+  end
+end
+""" |> to_source_file
+    |> assert_issues(@described_check, nil, &(length(&1) == 2))
+  end
+end
