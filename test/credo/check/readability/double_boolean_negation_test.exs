fe47d155e186e421e048dcbc5908931cb39c4e20
Julius Beckmann <julius.beckmann@digimondo.de>
2016-11-30 08:34:31 +0100

    Added two more tests and removed a unused variable warning.

--- test/credo/check/readability/double_boolean_negation_test.exs
+++ test/credo/check/readability/double_boolean_negation_test.exs
@@ -20,6 +20,22 @@ not true
     end)
   end
 
+  test "it should report a violation just once" do
+"""
+!!!true
+""" |> to_source_file
+    |> assert_issue(@described_check, fn(%Credo.Issue{trigger: trigger}) ->
+      assert "!!" == trigger
+    end)
+  end
+
+  test "it should report a violation twice" do
+"""
+!!!!true
+""" |> to_source_file
+    |> assert_issues(@described_check)
+  end
+
   test "it should report a violation 2" do
 """
 not not true
