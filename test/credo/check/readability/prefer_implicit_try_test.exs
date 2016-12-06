fef60e43ad8d7e74571fad1ccc2586602262f8d3
Devon Estes <devon.c.estes@gmail.com>
2016-12-06 13:59:03 +0100

    Update tests and implementation per review
    I had misunderstood the intent of the previous commit, so this commit updates
    this implementation to be in line with what was originally asked of me.

--- test/credo/check/readability/prefer_implicit_try_test.exs
+++ test/credo/check/readability/prefer_implicit_try_test.exs
@@ -3,7 +3,22 @@ defmodule Credo.Check.Readability.PreferImplicitTryTest do
 
   @described_check Credo.Check.Readability.PreferImplicitTry
 
-  test "it should NOT report expected code" do
+  test "it should report cases where a `try` block is the entire body of the function" do
+"""
+defmodule ModuleWithExplicitTry do
+   def failing_function(first) do
+     try do
+       to_string(first)
+     rescue
+        _ -> :rescued
+     end
+   end
+end
+""" |> to_source_file
+    |> assert_issue(@described_check)
+  end
+
+  test "it should NOT report implicit use of `try`" do
 """
 defmodule ModuleWithImplicitTry do
   def failing_function(first) do
@@ -16,51 +31,18 @@ end
     |> refute_issues(@described_check)
   end
 
-  test "it should report a violation" do
-"""
-defmodule ModuleWithExplicitTry do
-   def failing_function(first) do
-     try do
-       to_string(first)
-      rescue
-        _ -> :rescued
-     end
-   end
-end
-""" |> to_source_file
-    |> assert_issue(@described_check)
-  end
-
-  test "it should report a violation where try isn't first" do
-"""
-defmodule ModuleWithExplicitTry do
-   def failing_function(first) do
-     a = to_atom(first)
-
-     try do
-       to_string(first)
-      rescue
-        _ -> :rescued
-     end
-
-     [a, first]
-   end
-end
-""" |> to_source_file
-    |> assert_issue(@described_check)
-  end
-
   test "it should NOT report a violation in cases where we need `try`" do
 """
 defmodule ModuleWithExplicitTry do
    def failing_function(first) do
      other_function()
 
-     str = try do
-       to_string(first)
-      rescue
-        _ -> "rescued" 
-     end
+     str =
+       try do
+         to_string(first)
+       rescue
+           _ -> "rescued" 
+       end
 
      to_atom(string)
    end
