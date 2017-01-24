f5c7e1f0602e5b299f04466e2594ebc114abe579
Josh Price <josh@canoniq.com>
2017-01-24 19:22:31 +1100

    Add spec for #305

--- test/credo/check/readability/string_sigils_test.exs
+++ test/credo/check/readability/string_sigils_test.exs
@@ -19,6 +19,16 @@ defmodule Credo.Check.Readability.StringSigilsTest do
     """
   end
 
+  def create_heredoc_snippet(string_literal) do
+    """
+    defmodule CredoTest do
+      @module_var \"\"\"
+      #{string_literal}
+      \"\"\"
+    end
+    """
+  end
+
   #
   # cases NOT raising issues
   #
@@ -58,6 +68,13 @@ defmodule Credo.Check.Readability.StringSigilsTest do
     |> to_source_file
     |> refute_issues(@described_check)
   end
+
+  test "does NOT report for quotes in heredoc" do
+    create_heredoc_snippet(~s(f\\"\\"b\\"\\"))
+    |> to_source_file
+    |> refute_issues(@described_check)
+  end
+
 
   #
   # cases raising issues
