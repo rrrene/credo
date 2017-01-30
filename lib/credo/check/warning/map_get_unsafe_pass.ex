013738413576c50de3abbba3991b42f23e9e1a1c
Nicolas Arkkila <nicolas@arkkila.fi>
2017-01-30 23:06:14 +0200

    Refactor logic for finding unsafe Map.gets
    Plucked `to_function_call_name` here, also improved logic.
    The head is handled separately as it must always have 3 arguments
    if it's a `Map.get` call. Any consecutive `Map.get` inside a pipe
    must have at least two arguments.

--- lib/credo/check/warning/map_get_unsafe_pass.ex
+++ lib/credo/check/warning/map_get_unsafe_pass.ex
@@ -46,8 +46,8 @@ defmodule Credo.Check.Warning.MapGetUnsafePass do
     pipewalk(left) ++ pipewalk(List.first(right))
   end
 
-  defp pipewalk({{:., _, [{:__aliases__, _, [namespace]}, callable]}, _, args}) do
-    [{"#{namespace}.#{callable}", args}]
+  defp pipewalk({{:., _, [{:__aliases__, _, [namespace]}, callable]} = ast, _, args}) do
+    [{to_function_call_name(ast), args}]
   end
 
   defp pipewalk({value, _meta, args} = _ast) when is_atom(value),  do: [{value, args}]
@@ -56,20 +56,28 @@ defmodule Credo.Check.Warning.MapGetUnsafePass do
 
   defp is_unsafe?(expression) do
     len = Enum.count(expression) - 1
-    {_, expression} = List.pop_at(expression, len)
+    {_, [{head, head_arguments} | expression]} = List.pop_at(expression, len)
 
-    true in Enum.map(expression, fn x ->
-                                   case x do
-                                     {"Map.get", [head | _]} when is_atom(head) -> true
-                                     {"Map.get", [head | tail]} when is_tuple(head) -> Enum.count(tail) < 2
-                                     {"Map.get", [_]} -> true
-                                     _ -> false
-                                   end
-                                 end)
+    unsafe_head = (head == "Map.get" and length(head_arguments) != 3)
+
+    check = fn x ->
+              case x do
+               {"Map.get", [_]} -> true
+               _ -> false
+              end
+            end
+
+    true in Enum.map(expression, check) or unsafe_head
   end
 
   def issues_for_call(meta, issues, issue_meta) do
     [issue_for(issue_meta, meta[:line], @call_string) | issues]
+  end
+
+  defp to_function_call_name({_, _, _} = ast) do
+    {ast, [], []}
+    |> Macro.to_string()
+    |> String.replace(~r/\.?\(.*\)$/s, "")
   end
 
   defp issue_for(issue_meta, line_no, trigger) do
