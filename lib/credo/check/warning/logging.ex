000cd2018b4a7cd3064f4b0ae5c9400c034137e6
Alex Myasoedov <msoedov@gmail.com>
2017-02-21 19:18:51 -0500

    Check for lazy logging imported names

--- lib/credo/check/warning/logging.ex
+++ lib/credo/check/warning/logging.ex
@@ -1,35 +1,53 @@
 defmodule Credo.Check.Warning.Logging do
   @moduledoc """
+  Ensures laiziness of Logger calls.
   The best practice is to wrap an expensive logger expression into a zero argument function (fn -> "input" end)
+  Example:
+    Logger.debug fn ->
+        "my_fun/1: input: #{inspect :expensive_to_compute}"
+    end
+  Instead of:
+    Logger.debug "my_fun/1: input: #{inspect :expensive_to_compute}"
+
   """
 
   @explanation [check: @moduledoc]
-  @call_string "Logger.<>"
 
   use Credo.Check, base_priority: :high
 
   @doc false
   def run(%SourceFile{} = source_file, params \\ []) do
     issue_meta = IssueMeta.for(source_file, params)
-
     Credo.Code.prewalk(source_file, &traverse(&1, &2, issue_meta))
   end
 
   defp traverse({{:., _, [{:__aliases__, _, [:Logger]}, _]}, meta, arguments} = ast, issues, issue_meta) do
     {ast, issues_for_call(arguments, meta, issues, issue_meta)}
   end
+  defp traverse({:debug, meta, arguments} = ast, issues, issue_meta) do
+    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  end
+  defp traverse({:info, meta, arguments} = ast, issues, issue_meta) do
+    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  end
+  defp traverse({:warn, meta, arguments} = ast, issues, issue_meta) do
+    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  end
+  defp traverse({:error, meta, arguments} = ast, issues, issue_meta) do
+    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  end
   defp traverse(ast, issues, _issue_meta) do
     {ast, issues}
   end
 
-  def issues_for_call([{:fn, _meta, _args}] = args, _meta, issues, _issue_meta) do
+  def issues_for_call([{:fn, _, __}] = _args, _meta, issues, _issue_meta) do
     issues
   end
   def issues_for_call(_args, meta, issues, issue_meta) do
-    [issue_for(issue_meta, meta[:line], @call_string) | issues]
+    [issue_for(issue_meta, meta[:line]) | issues]
   end
 
-  defp issue_for(issue_meta, line_no, trigger) do
+  defp issue_for(issue_meta, line_no) do
     format_issue issue_meta,
       message: "Logger input is not lazzy",
       line_no: line_no
--- test/credo/check/warning/logging_test.exs
+++ test/credo/check/warning/logging_test.exs
@@ -20,6 +20,20 @@ end
     |> refute_issues(@described_check)
   end
 
+  test "it should NOT report imported Logger" do
+"""
+defmodule CredoSampleModule do
+  import Logger
+  def some_function(parameter1, parameter2) do
+    debug fn ->
+        "lazy/1: input: #{inspect 1}"
+    end
+  end
+end
+""" |> to_source_file
+    |> refute_issues(@described_check)
+  end
+
   #
   # cases raising issues
   #
@@ -29,6 +43,18 @@ end
 defmodule CredoSampleModule do
   def some_function(parameter1, parameter2) do
       Logger.debug "my_fun/1: input: #{inspect 1}"
+  end
+end
+""" |> to_source_file
+    |> assert_issue(@described_check)
+  end
+
+  test "it should report a violation with imported name" do
+"""
+defmodule CredoSampleModule do
+  import Logger
+  def some_function(parameter1, parameter2) do
+    debug "violation/1: input: #{inspect 1}"
   end
 end
 """ |> to_source_file
