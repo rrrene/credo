3eb0dabf111a53c07d4c009c36d43dc813e7ebb7
Alex Myasoedov <msoedov@gmail.com>
2017-02-26 22:35:19 -0500

    Tests to warn only for functions imported from Logger module.

--- test/credo/check/warning/logging_test.exs
+++ test/credo/check/warning/logging_test.exs
@@ -23,11 +23,28 @@ end
   test "it should NOT report imported :debug from Logger" do
 """
 defmodule CredoSampleModule do
-  import Logger
+  import Logger, only: debug
   def some_function(parameter1, parameter2) do
     debug fn ->
         "lazy/1: input: #{inspect 1}"
     end
+  end
+end
+""" |> to_source_file
+    |> refute_issues(@described_check)
+  end
+
+  test "it should NOT report for debug function" do
+"""
+defmodule CredoSampleModule do
+  import Enum
+
+  def debug(whatever) do
+    whatever
+  end
+
+  def some_function(parameter1, parameter2) do
+    debug "violation/1: input: #{inspect 1}"
   end
 end
 """ |> to_source_file
