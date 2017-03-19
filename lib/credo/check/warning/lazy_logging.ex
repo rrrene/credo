742b585f75380a379631f39b376700f78d0abe83
Alex Myasoedov <msoedov@gmail.com>
2017-03-19 18:00:08 -0400

    Check for Logger message interpolation

--- lib/credo/check/warning/lazy_logging.ex
+++ lib/credo/check/warning/lazy_logging.ex
@@ -16,60 +16,48 @@ defmodule Credo.Check.Warning.LazyLogging do
   @doc false
   def run(%SourceFile{} = source_file, params \\ []) do
     issue_meta = IssueMeta.for(source_file, params)
-    state = {false, []} # Logger seen ?, list of issues
+    state = {false, []} # Logger import seen ?, list of issues
     {_, issues} = Credo.Code.prewalk(source_file, &traverse(&1, &2, issue_meta), state)
     issues
   end
 
-  defp traverse({{:., _, [{:__aliases__, _, [:Logger]}, _]}, meta, arguments} = ast, issues, issue_meta) do
-    {ast, issues_for_call(arguments, meta, issues, issue_meta, logger_call: true)}
+  defp traverse({{:., _, [{:__aliases__, _, [:Logger]}, _]}, meta, arguments} = ast, state, issue_meta) do
+    {ast, issues_for_call(arguments, meta, state, issue_meta)}
   end
-  defp traverse({:debug, meta, arguments} = ast, issues, issue_meta) do
-    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  defp traverse({:debug, meta, arguments} = ast, {true, _issues} = state, issue_meta) do
+    {ast, issues_for_call(arguments, meta, state, issue_meta)}
   end
-  defp traverse({:info, meta, arguments} = ast, issues, issue_meta) do
-    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  defp traverse({:info, meta, arguments} = ast, {true, _issues} = state, issue_meta) do
+    {ast, issues_for_call(arguments, meta, state, issue_meta)}
   end
-  defp traverse({:warn, meta, arguments} = ast, issues, issue_meta) do
-    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  defp traverse({:warn, meta, arguments} = ast, {true, _issues} = state, issue_meta) do
+    {ast, issues_for_call(arguments, meta, state, issue_meta)}
   end
-  defp traverse({:error, meta, arguments} = ast, issues, issue_meta) do
-    {ast, issues_for_call(arguments, meta, issues, issue_meta)}
+  defp traverse({:error, meta, arguments} = ast, {true, _issues} = state, issue_meta) do
+    {ast, issues_for_call(arguments, meta, state, issue_meta)}
   end
-  defp traverse({:import, _meta, arguments} = ast, issues, _issue_meta) do
+  defp traverse({:import, _meta, arguments} = ast, state, _issue_meta) do
      if logger_import?(arguments) do
-        {_, issue_list} = issues
+        {_, issue_list} = state
         {ast, {true, issue_list}}
      else
-        {ast, issues}
+        {ast, state}
      end
   end
-  defp traverse(ast, issues, _issue_meta) do
-    {ast, issues}
+  defp traverse(ast, state, _issue_meta) do
+    {ast, state}
   end
 
-  defp issues_for_call([{:fn, _, __}] = _args, _meta, issues, _issue_meta, _) do
-    issues
+  defp issues_for_call([{:<<>>, _, [_ | _]} | _] = _args, meta, {import?, issues}, issue_meta) do
+      {import?, [issue_for(issue_meta, meta[:line]) | issues]}
   end
-  defp issues_for_call(args, meta, {true, issues}, issue_meta) do
-    #  arity check
-    case Enum.count(args) do
-        1 ->
-            {true, [issue_for(issue_meta, meta[:line]) | issues]}
-        _ ->
-            {true, issues}
-    end
-  end
-  defp issues_for_call(_args, meta, {import?, issues}, issue_meta, logger_call: true) do
-    {import?, [issue_for(issue_meta, meta[:line]) | issues]}
-  end
-  defp issues_for_call(_args, _meta, issues, _issue_meta) do
-    issues
+  defp issues_for_call(_args, _meta, state, _issue_meta) do
+    state
   end
 
   defp issue_for(issue_meta, line_no) do
     format_issue issue_meta,
-      message: "Logger call is not lazzy",
+      message: "Logger usage is not lazzy",
       line_no: line_no
   end
 
--- test/credo/check/warning/logging_test.exs
+++ test/credo/check/warning/logging_test.exs
@@ -7,12 +7,12 @@ defmodule Credo.Check.Warning.LazyLoggingTest do
   # cases NOT raising issues
   #
 
-  test "it should NOT report expected code" do
+  test "it should NOT report lazzy logging" do
 """
 defmodule CredoSampleModule do
   def some_function(parameter1, parameter2) do
     Logger.debug fn ->
-        "my_fun/1: input: #{inspect 1}"
+        "A debug message: #{inspect 1}"
     end
   end
 end
@@ -26,7 +26,7 @@ defmodule CredoSampleModule do
   import Logger, only: debug
   def some_function(parameter1, parameter2) do
     debug fn ->
-        "lazy/1: input: #{inspect 1}"
+        "Some message: #\{CredoSampleModule\}"
     end
   end
 end
@@ -44,7 +44,33 @@ defmodule CredoSampleModule do
   end
 
   def some_function(parameter1, parameter2) do
-    debug "violation/1: input: #{inspect 1}"
+    debug "Inspected: #\{CredoSampleModule\}"
+  end
+end
+""" |> to_source_file
+    |> refute_issues(@described_check)
+  end
+
+  test "it should NOT report for non interpolated strings" do
+"""
+defmodule CredoSampleModule do
+  def some_function do
+    Logger.debug "Hallo"
+  end
+end
+""" |> to_source_file
+    |> refute_issues(@described_check)
+  end
+
+  test "it should NOT report for function call" do
+"""
+defmodule CredoSampleModule do
+
+  def message do
+      "Imma message"
+  end
+  def some_function do
+    Logger.debug message
   end
 end
 """ |> to_source_file
@@ -55,11 +81,13 @@ end
   # cases raising issues
   #
 
-  test "it should report a violation" do
+  test "it should report a violation for interpolated strings" do
 """
 defmodule CredoSampleModule do
+
   def some_function(parameter1, parameter2) do
-      Logger.debug "my_fun/1: input: #{inspect 1}"
+      var_1 = "Hello world"
+      Logger.warn "The module: #\{var1\}"
   end
 end
 """ |> to_source_file
@@ -71,10 +99,11 @@ end
 defmodule CredoSampleModule do
   import Logger
   def some_function(parameter1, parameter2) do
-    debug "violation/1: input: #{inspect 1}"
+    debug "Ok #\{inspect 1\}"
   end
 end
 """ |> to_source_file
     |> assert_issue(@described_check)
   end
+
 end
