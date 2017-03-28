065991f98fb2b35697a9711512c2e0a646ce5393
Tim Huddle <blazhen88@gmail.com>
2017-03-28 17:12:38 -0400

    fixing tests to match what windows is actually doing

--- test/credo/cli/filename_test.exs
+++ test/credo/cli/filename_test.exs
@@ -3,12 +3,12 @@ defmodule Credo.CLI.FilenameTest do
   doctest Credo.CLI.Filename
 
   test "the truth" do
-    assert "C:\\Credo\\sources.ex" == Credo.CLI.Filename.remove_line_no_and_column("C:\\Credo\\sources.ex:39:8")
+    assert "C:/Credo/sources.ex" == Credo.CLI.Filename.remove_line_no_and_column("C:/Credo/sources.ex:39:8")
   end
 
   test "contains line no" do
-    assert true == Credo.CLI.Filename.contains_line_no?("C:\\Credo\\sources.ex:39:8")
-    assert true == Credo.CLI.Filename.contains_line_no?("C:\\Credo\\sources.ex:39")
-    assert false == Credo.CLI.Filename.contains_line_no?("C:\\Credo\\sources.ex")
+    assert true == Credo.CLI.Filename.contains_line_no?("C:/Credo/sources.ex:39:8")
+    assert true == Credo.CLI.Filename.contains_line_no?("C:/Credo/sources.ex:39")
+    assert false == Credo.CLI.Filename.contains_line_no?("C:/Credo/sources.ex")
   end
 end
