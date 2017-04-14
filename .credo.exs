2ecf7aadd4abf330c848461e4e962648b6751608
René Föhring <rene.foehring@5minds.de>
2017-04-14 22:24:38 +0200

    Add LazyLogging to .credo.exs

--- .credo.exs
+++ .credo.exs
@@ -104,6 +104,7 @@
         {Credo.Check.Warning.BoolOperationOnSameValues},
         {Credo.Check.Warning.IExPry},
         {Credo.Check.Warning.IoInspect},
+        {Credo.Check.Warning.LazyLogging},
         {Credo.Check.Warning.OperationOnSameValues},
         {Credo.Check.Warning.OperationWithConstantResult},
         {Credo.Check.Warning.UnusedEnumOperation},
