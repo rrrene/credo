0114d028fb39ff2a831b1002ce77d45629cdbced
René Föhring <rf@bamaru.de>
2017-05-21 19:53:53 +0200

    Bump version to 0.8.0-rc6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Credo.Mixfile do
   def project do
     [
       app: :credo,
-      version: "0.8.0-rc5",
+      version: "0.8.0-rc6",
       elixir: "~> 1.1",
       escript: [main_module: Credo.CLI],
       build_embedded: Mix.env == :prod,
