2549e94d2faa3fc66088e3ca9e632e93cdcabf8c
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2017-08-14 13:51:57 -0300

    Guard against unquoting attributes on `ModuleAttributeNames`.

--- test/credo/check/readability/module_attribute_names_test.exs
+++ test/credo/check/readability/module_attribute_names_test.exs
@@ -16,6 +16,19 @@ end
     |> refute_issues(@described_check)
   end
 
+  test "it should NOT fail on a dynamic attribute" do
+"""
+defmodule CredoSampleModule do
+  defmacro define(key, value) do
+    quote do
+      @unquote(key)(unquote(value))
+    end
+  end
+end
+""" |> to_source_file
+    |> refute_issues(@described_check)
+  end
+
   #
   # cases raising issues
   #
