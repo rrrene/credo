2057a4835e2c36ec5076a94468c54cc6aa5de252
René Föhring <rf@bamaru.de>
2017-10-08 15:35:56 +0200

    Adjust minimal Elixir version for LargeNumbers test (again)

--- test/credo/check/readability/large_numbers_test.exs
+++ test/credo/check/readability/large_numbers_test.exs
@@ -7,7 +7,7 @@ defmodule Credo.Check.Readability.LargeNumbersTest do
   # cases NOT raising issues
   #
 
-  @module_tag needs_elixir: "1.3.2"
+  @moduletag needs_elixir: "1.3.2"
 
   test "it should NOT report expected code" do
 """
