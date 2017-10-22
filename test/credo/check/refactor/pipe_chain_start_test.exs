5e5f3fb9076d4f7e93c48705439c754546504e5b
jdollar <dollarhidejoel@gmail.com>
2017-10-22 00:11:39 -0400

    Adding test for no issues with a pipe that starts on a erlang function

--- test/credo/check/refactor/pipe_chain_start_test.exs
+++ test/credo/check/refactor/pipe_chain_start_test.exs
@@ -183,6 +183,13 @@ put_in(users["john"][:age], 28)
     |> refute_issues(@described_check, excluded_functions: ~w(:crypto.hash))
   end
 
+  test "it should NOT report a violation for a erlang function call" do
+"""
+:crypto.hash(:md5, "test") |> Base.encode16(case: :lower)
+""" |> to_source_file
+    |> refute_issues(@described_check, exclude_erlang_funcs: true)
+  end
+
   test "it should NOT report a violation for ++" do
 """
   def build(%Ecto.Query{} = query) do
