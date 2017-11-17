c97f0ade29a5c157983a0dd243072ee2e707d387
René Föhring <rf@bamaru.de>
2017-11-17 08:40:27 +0100

    Update docs for ExpensiveEmptyEnumCheck

--- lib/credo/check/warning/expensive_empty_enum_check.ex
+++ lib/credo/check/warning/expensive_empty_enum_check.ex
@@ -1,7 +1,16 @@
 defmodule Credo.Check.Warning.ExpensiveEmptyEnumCheck do
   @moduledoc """
-  Checking if an enum is empty should be done with `Enum.empty?(enum)` or
-  `list == []`. Checking if the size of the enum is `0` can be very expensive.
+  Checking if the size of the enum is `0` can be very expensive, since you are
+  determining the exact count of elements.
+
+  Checking if an enum is empty should be done by using
+
+    Enum.empty?(enum)
+
+  or
+
+    list == []
+
   """
 
   @explanation [check: @moduledoc]
