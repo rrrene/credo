ead0be8284b31ef5269cb1fdc4cb5a706c46460b
René Föhring <rf@bamaru.de>
2018-01-04 17:13:13 +0100

    Format check utility modules

--- lib/credo/check/runner.ex
+++ lib/credo/check/runner.ex
@@ -7,28 +7,30 @@ defmodule Credo.Check.Runner do
   @doc false
   def run(source_files, exec) when is_list(source_files) do
     {_time_run_on_all, _source_files_after_run_on_all} =
-      :timer.tc fn ->
+      :timer.tc(fn ->
         run_checks_that_run_on_all(source_files, exec)
-      end
+      end)
 
     {_time_run, _source_files} =
-      :timer.tc fn ->
+      :timer.tc(fn ->
         source_files
         |> Enum.map(&Task.async(fn -> run(&1, exec) end))
         |> Enum.map(&Task.await(&1, :infinity))
-      end
+      end)
 
     :ok
   end
+
   def run(%SourceFile{} = source_file, exec) do
     checks =
       exec
-      |> Execution.checks
+      |> Execution.checks()
       |> Enum.reject(&run_on_all_check?/1)
 
     case run_checks(source_file, checks, exec) do
       [] ->
         nil
+
       issues ->
         Issues.append(exec, source_file, issues)
     end
@@ -50,36 +52,42 @@ defmodule Credo.Check.Runner do
     |> Enum.into(%{})
   end
 
-
   defp run_checks_that_run_on_all(source_files, exec) do
     checks =
       exec
-      |> Execution.checks
+      |> Execution.checks()
       |> Enum.filter(&run_on_all_check?/1)
 
     checks
-    |> Enum.map(&Task.async(fn ->
+    |> Enum.map(
+      &Task.async(fn ->
         run_check(&1, source_files, exec)
-      end))
+      end)
+    )
     |> Enum.each(&Task.await(&1, :infinity))
 
     :ok
   end
 
-  defp run_checks(%SourceFile{} = source_file, checks, exec) when is_list(checks) do
+  defp run_checks(%SourceFile{} = source_file, checks, exec)
+       when is_list(checks) do
     Enum.flat_map(checks, &run_check(&1, source_file, exec))
   end
 
   # Returns issues
-  defp run_check({_check, false}, source_files, _exec) when is_list(source_files) do
+  defp run_check({_check, false}, source_files, _exec)
+       when is_list(source_files) do
     source_files
   end
+
   defp run_check({_check, false}, _source_file, _exec) do
     []
   end
+
   defp run_check({check}, source_file, exec) do
     run_check({check, []}, source_file, exec)
   end
+
   defp run_check({check, params}, source_files, exec) when is_list(source_files) do
     try do
       check.run(source_files, exec, params)
@@ -94,6 +102,7 @@ defmodule Credo.Check.Runner do
         end
     end
   end
+
   defp run_check({check, params}, source_file, exec) do
     try do
       check.run(source_file, params)
@@ -112,6 +121,7 @@ defmodule Credo.Check.Runner do
   defp warn_about_failed_run(check, %SourceFile{} = source_file) do
     UI.warn("Error while running #{check} on #{source_file.filename}")
   end
+
   defp warn_about_failed_run(check, _) do
     UI.warn("Error while running #{check}")
   end
