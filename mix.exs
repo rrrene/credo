b2d8cf111d74532b072a016f9dde1b79b00916d7
René Föhring <rf@bamaru.de>
2018-01-07 09:19:40 +0100

    Set Elixir version to >= 1.3.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Credo.Mixfile do
     [
       app: :credo,
       version: "0.9.0-rc1",
-      elixir: "~> 1.1",
+      elixir: ">= 1.3.2",
       escript: [main_module: Credo.CLI],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
