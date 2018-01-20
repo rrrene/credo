6089bd528ea8ed2a1e2b7063db25bac27c270e12
Dylan Chong <dylan.chong@icloud.com>
2018-01-20 13:41:07 +1300

    Include test directory in files that can be limited

--- .credo.exs
+++ .credo.exs
@@ -21,7 +21,7 @@
         # You can give explicit globs or simply directories.
         # In the latter case `**/*.{ex,exs}` will be used.
         #
-        included: ["lib/", "src/", "web/", "apps/"],
+        included: ["lib/", "src/", "test/", "web/", "apps/"],
         excluded: [~r"/_build/", ~r"/deps/"]
       },
       #
