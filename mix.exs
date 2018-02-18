07ba4a8ea2135aef7f0dc0542a261c0e07e200c1
René Föhring <rf@bamaru.de>
2018-02-18 14:14:23 +0100

    Fix Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Credo.Mixfile do
     [
       app: :credo,
       version: "0.9.0-rc4",
-      elixir: ">= 1.4",
+      elixir: ">= 1.4.0",
       escript: [main_module: Credo.CLI],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
