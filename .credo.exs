b91b7bae50fb3fddfd62cf479be4dfcc7cd52910
René Föhring <rf@bamaru.de>
2018-06-26 11:29:16 +0200

    Add node_modules to default excludes

--- .credo.exs
+++ .credo.exs
@@ -22,7 +22,7 @@
         # In the latter case `**/*.{ex,exs}` will be used.
         #
         included: ["lib/", "src/", "test/", "web/", "apps/"],
-        excluded: [~r"/_build/", ~r"/deps/"]
+        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
       },
       #
       # If you create your own checks, you must specify the source files for
