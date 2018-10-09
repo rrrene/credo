de4d0ef4fc379d4b5da86de19ac7e2bb9f30c4c4
Sean Callan <seancallan@gmail.com>
2018-10-09 17:10:57 -0600

    Fix code duplication with new UnusedOperation module

--- lib/credo/check/warning/unused_enum_operation.ex
+++ lib/credo/check/warning/unused_enum_operation.ex
@@ -26,55 +26,25 @@ defmodule Credo.Check.Warning.UnusedEnumOperation do
   (the exception being `Enum.each/2` which iterates a list and returns `:ok`).
   """
 
+  use Credo.Check, base_priority: :high
+
+  alias Credo.Check.Warning.UnusedOperation
+
   @explanation [check: @moduledoc]
   @checked_module :Enum
   @funs_with_return_value ~w(
-    all? any? at chunk chunk chunk_by concat concat count count dedup
-    dedup_by drop drop_while empty? fetch fetch! filter filter_map
-    find find_index find_value flat_map flat_map_reduce group_by
-    intersperse into into join map map_join map_reduce max max_by
-    member min min_by min_max min_max_by partition random reduce
-    reduce reduce_while reject reverse reverse reverse_slice scan
-    scan shuffle slice slice sort sort sort_by split split_while
-    sum take take_every take_random take_while to_list uniq uniq_by
-    unzip with_index zip
-  )a
+      all? any? at chunk chunk chunk_by concat concat count count dedup
+      dedup_by drop drop_while empty? fetch fetch! filter filter_map
+      find find_index find_value flat_map flat_map_reduce group_by
+      intersperse into into join map map_join map_reduce max max_by
+      member min min_by min_max min_max_by partition random reduce
+      reduce reduce_while reject reverse reverse reverse_slice scan
+      scan shuffle slice slice sort sort sort_by split split_while
+      sum take take_every take_random take_while to_list uniq uniq_by
+      unzip with_index zip
+    )a
 
-  alias Credo.Check.Warning.UnusedFunctionReturnHelper
-
-  use Credo.Check, base_priority: :high
-
-  @doc false
   def run(source_file, params \\ []) do
-    issue_meta = IssueMeta.for(source_file, params)
-
-    all_unused_calls =
-      UnusedFunctionReturnHelper.find_unused_calls(
-        source_file,
-        params,
-        [@checked_module],
-        @funs_with_return_value
-      )
-
-    Enum.reduce(all_unused_calls, [], fn invalid_call, issues ->
-      {_, meta, _} = invalid_call
-
-      trigger =
-        invalid_call
-        |> Macro.to_string()
-        |> String.split("(")
-        |> List.first()
-
-      issues ++ [issue_for(issue_meta, meta[:line], trigger)]
-    end)
-  end
-
-  defp issue_for(issue_meta, line_no, trigger) do
-    format_issue(
-      issue_meta,
-      message: "There should be no unused return values for #{trigger}().",
-      trigger: trigger,
-      line_no: line_no
-    )
+    UnusedOperation.run(source_file, params, @checked_module, @funs_with_return_value, &format_issue/2)
   end
 end
--- lib/credo/check/warning/unused_regex_operation.ex
+++ lib/credo/check/warning/unused_regex_operation.ex
@@ -8,45 +8,14 @@ defmodule Credo.Check.Warning.UnusedRegexOperation do
   variable which has to be used somehow.
   """
 
-  @explanation [check: @moduledoc]
-  @checked_module :Regex
-  @funs_with_return_value nil
-
-  alias Credo.Check.Warning.UnusedFunctionReturnHelper
-
   use Credo.Check, base_priority: :high
 
-  @doc false
+  alias Credo.Check.Warning.UnusedOperation
+
+  @explanation [check: @moduledoc]
+  @checked_module :Regex
+
   def run(source_file, params \\ []) do
-    issue_meta = IssueMeta.for(source_file, params)
-
-    all_unused_calls =
-      UnusedFunctionReturnHelper.find_unused_calls(
-        source_file,
-        params,
-        [@checked_module],
-        @funs_with_return_value
-      )
-
-    Enum.reduce(all_unused_calls, [], fn invalid_call, issues ->
-      {_, meta, _} = invalid_call
-
-      trigger =
-        invalid_call
-        |> Macro.to_string()
-        |> String.split("(")
-        |> List.first()
-
-      issues ++ [issue_for(issue_meta, meta[:line], trigger)]
-    end)
-  end
-
-  defp issue_for(issue_meta, line_no, trigger) do
-    format_issue(
-      issue_meta,
-      message: "There should be no unused return values for #{trigger}().",
-      trigger: trigger,
-      line_no: line_no
-    )
+    UnusedOperation.run(source_file, params, @checked_module, [], &format_issue/2)
   end
 end
