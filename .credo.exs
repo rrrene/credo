182ae5fcb3a234e4da2bebe9382f984413de70b0
Michał Kalbarczyk <fazibear@gmail.com>
2018-10-18 19:19:55 +0200

    Update .credo.exs

--- .credo.exs
+++ .credo.exs
@@ -21,7 +21,7 @@
         # You can give explicit globs or simply directories.
         # In the latter case `**/*.{ex,exs}` will be used.
         #
-        included: ["lib/", "src/", "test/", "web/", "apps/", "dupa"],
+        included: ["lib/", "src/", "test/", "web/", "apps/"],
         excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
       },
       #
