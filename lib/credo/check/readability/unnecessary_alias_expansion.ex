114588720c082b72306fc4b722180dd8f3e97b6c
Rémi Prévost <remi@exomel.com>
2019-03-07 07:01:21 -0500

    Add UnnecessaryAliasExpansion check

---
+++ lib/credo/check/readability/unnecessary_alias_expansion.ex
@@ -0,0 +1,48 @@
+defmodule Credo.Check.Readability.UnnecessaryAliasExpansion do
+  @moduledoc false
+
+  @checkdoc """
+  Alias expansion is useful but when aliasing a single module,
+  it can be harder to read with unnecessary braces.
+
+      # preferred
+
+      alias ModuleA.Foo
+      alias ModuleA.{Foo, Bar}
+
+      # NOT preferred
+
+      alias ModuleA.{Foo}
+
+  Like all `Readability` issues, this one is not a technical concern.
+  But you can improve the odds of others reading and liking your code by making
+  it easier to follow.
+  """
+  @explanation [check: @checkdoc]
+
+  use Credo.Check, base_priority: :low
+
+  alias Credo.Code
+
+  @doc false
+  def run(source_file, params \\ []) do
+    issue_meta = IssueMeta.for(source_file, params)
+
+    Code.prewalk(source_file, &traverse(&1, &2, issue_meta))
+  end
+
+  defp traverse({:alias, _, [{_, _, [{:__aliases__, opts, [child]}]}]} = ast, issues, issue_meta) do
+    {ast, issues ++ [issue_for(issue_meta, Keyword.get(opts, :line), child)]}
+  end
+
+  defp traverse(ast, issues, _issue_meta), do: {ast, issues}
+
+  defp issue_for(issue_meta, line_no, trigger) do
+    format_issue(
+      issue_meta,
+      message: "Unnecessary alias expansion for #{trigger}, consider removing braces.",
+      trigger: trigger,
+      line_no: line_no
+    )
+  end
+end
