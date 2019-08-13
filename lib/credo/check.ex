c3813db4725247664e98270b4a91ea1eae61036e
Saša Jurić <sasa.juric@gmail.com>
2019-08-13 15:14:39 +0200

    Fix type specification
    A wrong typespec causes dialyzer errors in a client project where custom credo checks are built.

--- lib/credo/check.ex
+++ lib/credo/check.ex
@@ -36,7 +36,7 @@ defmodule Credo.Check do
 
   @callback explanation_for_params() :: Keyword.t()
 
-  @callback format_issue(issue_meta :: IssueMeta, opts :: Keyword.t()) :: Issue.t()
+  @callback format_issue(issue_meta :: Credo.IssueMeta.t(), opts :: Keyword.t()) :: Issue.t()
 
   @base_category_exit_status_map %{
     consistency: 1,
