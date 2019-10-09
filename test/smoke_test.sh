19c1a49ae23032ef9526961651cd872fb68dd49f
René Föhring <rf@bamaru.de>
2019-10-09 10:41:26 +0200

    Update smoke tests

--- test/smoke_test.sh
+++ test/smoke_test.sh
@@ -11,8 +11,10 @@ mix credo --debug --mute-exit-status
 mix credo list --mute-exit-status
 mix credo suggest --mute-exit-status
 
-mix credo lib/credo/sources.ex:1:11 --mute-exit-status
-mix credo explain lib/credo/sources.ex:1:11 --mute-exit-status
+mix credo test/fixtures/example_code/clean_redux.ex:1:11 --mute-exit-status
+mix credo explain test/fixtures/example_code/clean_redux.ex:1:11 --mute-exit-status
+mix credo test/fixtures/example_code/clean_redux.ex:1:11 --mute-exit-status --format=json
+mix credo explain test/fixtures/example_code/clean_redux.ex:1:11 --mute-exit-status --format=json
 
 mix credo.gen.check $GENEREATE_CREDO_CHECK
 rm $GENEREATE_CREDO_CHECK
@@ -20,10 +22,13 @@ rm $GENEREATE_CREDO_CHECK
 mix credo.gen.config
 
 mix credo categories
+mix credo categories --format=json
+
 mix credo info
 mix credo info --verbose
 mix credo info --format=json
 mix credo info --verbose --format=json
+
 mix credo version
 mix credo help
 
