7145dbd6ad37477aadc4ddd9c07241f3f1001a5d
René Föhring <rf@bamaru.de>
2020-02-15 11:17:45 +0100

    Fix replace_space in Code.Charlists

--- test/credo/code/charlists_test.exs
+++ test/credo/code/charlists_test.exs
@@ -11,7 +11,7 @@ defmodule Credo.Code.CharlistsTest do
     # '
 
     \"\"\"
-    y = 'also: # TODO: no comment here'
+    inside_heredoc = 'also: # TODO: no comment here'
     \"\"\"
 
     'also: # TODO: no comment here as well'
@@ -24,7 +24,7 @@ defmodule Credo.Code.CharlistsTest do
     # '
 
     \"\"\"
-    y = 'also: # TODO: no comment here'
+    inside_heredoc = 'also: # TODO: no comment here'
     \"\"\"
 
     '                                     '
