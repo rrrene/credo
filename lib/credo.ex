7b20e759e9766f6bbe1f09919511cb12f657ba39
René Föhring <rene.foehring@5minds.de>
2020-05-10 17:08:29 +0200

    WIP: Add Watcher

--- lib/credo.ex
+++ lib/credo.ex
@@ -8,7 +8,28 @@ defmodule Credo do
   - `Credo.Code` - all analysis tools used by Credo during analysis
   """
 
+  alias Credo.Execution
+  alias Credo.Execution.Task.WriteDebugReport
+
   @version Mix.Project.config()[:version]
+
+  @doc """
+  Runs Credo with the given `argv` and returns its final `Credo.Execution` struct.
+
+  Example:
+
+      iex> exec = Credo.CLI.run(["--only", "Readability"])
+      iex> issues = Credo.Execution.get_issues(exec)
+      iex> Enum.count(issues) > 0
+      true
+
+  """
+  def run(argv) do
+    argv
+    |> Execution.build()
+    |> Execution.run()
+    |> WriteDebugReport.call([])
+  end
 
   def version, do: @version
 end
--- lib/credo/cli.ex
+++ lib/credo/cli.ex
@@ -3,86 +3,26 @@ defmodule Credo.CLI do
   `Credo.CLI` is the entrypoint for both the Mix task and the escript.
   """
 
-  alias Credo.CLI.Output.UI
   alias Credo.Execution
-  alias Credo.Execution.Task.WriteDebugReport
 
   @doc """
   Runs Credo with the given `argv` and exits the process.
 
-  See `run/1` if you want to run Credo programmatically.
+  See `Credo.run/1` if you want to run Credo programmatically.
   """
   def main(argv \\ []) do
     Credo.Application.start(nil, nil)
 
-    {options, _, _} = OptionParser.parse(argv, strict: [watch: :boolean])
+    {options, argv_rest, _errors} = OptionParser.parse(argv, strict: [watch: :boolean])
 
     if options[:watch] do
-      UI.puts([:bright, :magenta, "watch: ", :reset, :faint, "Now watching for changes ...\n"])
-
-      argv
-      |> get_path()
-      |> start_watcher()
-
-      watch_loop(argv, nil)
+      Credo.Watcher.run(argv)
     else
       argv
-      |> run()
+      |> Credo.run()
       |> halt_if_exit_status_assigned()
     end
   end
-
-  @doc """
-  Runs Credo with the given `argv` and returns its final `Credo.Execution` struct.
-
-  Example:
-
-      iex> exec = Credo.CLI.run(["--only", "Readability"])
-      iex> issues = Credo.Execution.get_issues(exec)
-      iex> Enum.count(issues) > 0
-      true
-
-  """
-  def run(argv) do
-    argv
-    |> Execution.build()
-    |> Execution.run()
-    |> WriteDebugReport.call([])
-  end
-
-  defp start_watcher(path) do
-    {:ok, pid} = FileSystem.start_link(dirs: [path])
-    FileSystem.subscribe(pid)
-  end
-
-  defp watch_loop(argv, exec_from_last_run) do
-    receive do
-      {:file_event, _worker_pid, {file_path, events}} ->
-        elixir_file? = file_path =~ ~r/\.exs?$/
-        in_ignored_directory? = file_path =~ ~r/\/deps\/$/
-        relative_path = Path.relative_to_cwd(file_path)
-
-        exec =
-          if Enum.member?(events, :closed) && elixir_file? && !in_ignored_directory? do
-            UI.puts([:bright, :magenta, "watch: ", :reset, :faint, relative_path, "\n"])
-
-            run(argv)
-          else
-            UI.puts([
-              :bright,
-              :magenta,
-              "changes: ",
-              :reset,
-              :faint,
-              inspect({System.os_time(:milliseconds), events, relative_path})
-            ])
-          end
-
-        watch_loop(argv, exec || exec_from_last_run)
-    end
-  end
-
-  defp get_path(_argv), do: "."
 
   defp halt_if_exit_status_assigned(%Execution{mute_exit_status: true}) do
     # Skip if exit status is muted
