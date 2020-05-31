251011d27bae683251ad3f1bedd7466b1b52c4c1
Bryan Paxton <starbelly@pobox.com>
2020-05-31 00:56:44 -0500

    add initial doc and contrived example

--- lib/credo/check/readability/expr_pipe.ex
+++ lib/credo/check/readability/expr_pipe.ex
@@ -6,7 +6,43 @@ defmodule Credo.Check.Readability.ExprPipe do
       check: """
       Pipes (`|>`) should not be used with case, if, or unless expressions.
 
-      # TODO: Better docs here.
+      The code in this example ...
+
+          list
+          |> Enum.take(5)
+          |> Enum.sort()
+          |> case do 
+            [[_h|_t]|_] -> true
+            _ -> false
+          end
+
+      ... should be refactored to look like this:
+
+          maybe_nested_lists = list
+                               |> Enum.take(5)
+                               |> Enum.sort()
+
+          case maybe_nested_lists do 
+            [[_h|_t]|_] = true
+            _->  false
+          end
+
+
+      ... or create a new function
+
+          list
+          |> Enum.take(5)
+          |> Enum.sort()
+          |> contains_nested_list?()
+
+
+      Piping to expressions is harder to read because it may obscure intention, increase cognitive load on the 
+      reader, and suprising to the reader per not following basic syntax principles set forth by all other 
+      expressions. Instead, prefer introducing variables to your code or new functions when it may be a sign that 
+      your function is getting too big and/or has too many concerns. 
+
+      Like all `Readability` issues, this one is not a technical concern, but you can improve the odds of others reading
+      and understanding the intent of your code by making it easier to follow. 
       """
     ]
 
