4485002d8486f9bf967fa6d076898c7b468841bc
René Föhring <rene.foehring@5minds.de>
2020-07-25 15:44:32 +0200

    Fix compiler warnings

--- test/integration/explain_test.exs
+++ test/integration/explain_test.exs
@@ -7,16 +7,16 @@ defmodule Credo.ExplainTest do
 
   test "it should explain an issue using a filename with location" do
     exec = Credo.CLI.run([@fixture_example_code_with_location])
-    issues = Credo.Execution.get_issues(exec)
+    _issues = Credo.Execution.get_issues(exec)
 
     assert exec.cli_options.command == "explain"
     # TODO: how do we assert this?
-    # assert issues == []
+    # assert _issues == []
   end
 
   test "it should explain an issue using explain command" do
     exec = Credo.CLI.run(["explain", @fixture_example_code_with_location])
-    issues = Credo.Execution.get_issues(exec)
+    _issues = Credo.Execution.get_issues(exec)
 
     assert exec.cli_options.command == "explain"
     # TODO: how do we assert this?
@@ -25,7 +25,7 @@ defmodule Credo.ExplainTest do
 
   test "it should explain a check using explain command" do
     exec = Credo.CLI.run(["explain", @fixture_example_code_with_location])
-    issues = Credo.Execution.get_issues(exec)
+    _issues = Credo.Execution.get_issues(exec)
 
     assert exec.cli_options.command == "explain"
     # TODO: how do we assert this?
