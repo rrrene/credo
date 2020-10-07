052c633148a34b5480872fc8dc6981120f25ccd1
René Föhring <rene.foehring@5minds.de>
2020-10-07 12:05:05 +0200

    Try to reproduce #814

--- test/credo/check/consistency/multi_alias_import_require_use_test.exs
+++ test/credo/check/consistency/multi_alias_import_require_use_test.exs
@@ -10,6 +10,12 @@ defmodule Credo.Check.Consistency.MultiAliasImportRequireUseTest do
     require Foo.Bar
   end
   """
+  @single2 """
+  defmodule Credo.Sample2 do
+    import Assertions
+    import MyApp.Factory
+  end
+  """
   @multi """
   defmodule Credo.Sample3 do
     alias Foo.{Bar, Quux}
@@ -23,8 +29,22 @@ defmodule Credo.Check.Consistency.MultiAliasImportRequireUseTest do
   # cases NOT raising issues
   #
 
-  test "it should not report errors when the multi syntax is used consistently" do
+  test "it should NOT report errors when the multi syntax is used consistently" do
     [@multi]
+    |> to_source_files
+    |> run_check(@described_check)
+    |> refute_issues()
+  end
+
+  test "it should NOT report errors when the single syntax is used consistently" do
+    [@single]
+    |> to_source_files
+    |> run_check(@described_check)
+    |> refute_issues()
+  end
+
+  test "it should NOT report errors when the single syntax is used consistently /2" do
+    [@single, @single2]
     |> to_source_files
     |> run_check(@described_check)
     |> refute_issues()
