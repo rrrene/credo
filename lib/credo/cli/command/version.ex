e388019fcd5d2a1d322b8c50b5ecf1e25beb7218
René Föhring <rene.foehring@5minds.de>
2020-11-29 11:16:55 +0100

    Fix --version not honoring --format

--- lib/credo/cli/command/version.ex
+++ lib/credo/cli/command/version.ex
@@ -5,9 +5,17 @@ defmodule Credo.CLI.Command.Version do
 
   use Credo.CLI.Command
 
+  alias Credo.CLI.Output.Formatter.JSON
   alias Credo.CLI.Output.UI
+  alias Credo.Execution
 
   @doc false
+  def call(%Execution{format: "json"} = exec, _opts) do
+    JSON.print_map(%{version: Credo.version()})
+
+    exec
+  end
+
   def call(exec, _opts) do
     UI.puts(Credo.version())
 
