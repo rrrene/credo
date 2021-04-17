cb1367b9bcdc9c1a9dc1b7fb654958c7f557a523
Arno Dirlam <arnodirlam@googlemail.com>
2021-04-17 17:25:37 +0200

    GitHub format: Add groups, Add file location on new line

--- lib/credo/cli/output/formatter/github.ex
+++ lib/credo/cli/output/formatter/github.ex
@@ -1,16 +1,25 @@
 defmodule Credo.CLI.Output.Formatter.GitHub do
   @moduledoc false
 
+  alias Credo.CLI.Filename
   alias Credo.CLI.Output
   alias Credo.CLI.Output.UI
   alias Credo.Issue
 
   def print_issues(issues) do
-    Enum.each(issues, fn issue ->
-      issue
-      |> to_github()
-      |> UI.puts()
-    end)
+    issues
+    |> Enum.group_by(& &1.filename)
+    |> Enum.flat_map(&to_github_group/1)
+    |> Enum.each(&UI.puts/1)
+  end
+
+  def to_github_group({filename, issues}) do
+    count = length(issues)
+    count_issues = if count == 1, do: "1 issue", else: "#{count} issues"
+
+    ["::group::#{count_issues} in #{filename}"] ++
+      Enum.flat_map(issues, &to_github/1) ++
+      ["::endgroup::"]
   end
 
   def to_github(
@@ -18,13 +27,16 @@ defmodule Credo.CLI.Output.Formatter.GitHub do
           message: message,
           filename: filename,
           column: column,
-          line_no: line_no,
-          priority: priority
+          line_no: line_no
         } = issue
       ) do
-    tag = Output.check_tag(issue, false)
-    priority = priority |> Output.priority_arrow()
+    category = issue.category |> to_string() |> String.capitalize()
+    arrow = issue.priority |> Output.priority_arrow()
+    pos_suffix = Filename.pos_suffix(line_no, column)
 
-    "::error file=#{filename},line=#{line_no},col=#{column}::#{tag} #{priority} #{message}"
+    [
+      "::error file=#{filename},line=#{line_no},col=#{column}::#{message} [#{arrow} #{category}]",
+      "  at #{filename}#{pos_suffix}"
+    ]
   end
 end
