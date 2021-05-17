25575c8e2ba021050fec22e58d03d162432d3632
Marcus Kruse <esurk.sucram@gmail.com>
2021-05-17 19:52:20 +0200

    Update docs
    Co-authored-by: René Föhring <rf@bamaru.de>

--- lib/credo/check/refactor/useless_with_clause.ex
+++ lib/credo/check/refactor/useless_with_clause.ex
@@ -6,7 +6,7 @@ defmodule Credo.Check.Refactor.UselessWithClause do
       `with` statements are useful when you need to chain a sequence
       of pattern matches, stopping at the first one that fails.
 
-      If the `match` of the last clause in a with equal to the expression in the
+      If the match of the last clause in a `with` statement is identical to the expression in the
       in its body, the code should be refactored to remove the redundant expression.
 
       This should be refactored:
