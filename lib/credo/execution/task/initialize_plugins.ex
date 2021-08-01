ed3c59f872c1734f8fb2b0df743524873a12e3ef
René Föhring <rf@bamaru.de>
2021-08-01 17:39:20 +0200

    Fail if a plugin module can not be initialized

--- lib/credo/execution/task/initialize_plugins.ex
+++ lib/credo/execution/task/initialize_plugins.ex
@@ -1,6 +1,8 @@
 defmodule Credo.Execution.Task.InitializePlugins do
   @moduledoc false
 
+  use Credo.Execution.Task
+
   alias Credo.Execution
 
   def call(exec, _opts) do
@@ -10,10 +12,19 @@ defmodule Credo.Execution.Task.InitializePlugins do
   defp init_plugin(exec, {_mod, false}), do: exec
 
   defp init_plugin(exec, {mod, _params}) do
-    exec
-    |> Execution.set_initializing_plugin(mod)
-    |> mod.init()
-    |> Execution.ensure_execution_struct("#{mod}.init/1")
-    |> Execution.set_initializing_plugin(nil)
+    module_loaded? = function_exported?(mod, :__info__, 1)
+
+    if module_loaded? do
+      exec
+      |> Execution.set_initializing_plugin(mod)
+      |> mod.init()
+      |> Execution.ensure_execution_struct("#{mod}.init/1")
+      |> Execution.set_initializing_plugin(nil)
+    else
+      Execution.halt(
+        exec,
+        "Plugin module `#{Credo.Code.Module.name(mod)}` is not available and could not be initialized."
+      )
+    end
   end
 end
