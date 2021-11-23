61180c28ce106a4a3f1dcacc220f0e2eba30fc5f
ryvasquez <ryanannatuvasquez@gmail.com>
2021-11-23 20:35:48 +0800

    FIX: detect unused variable when using guards

--- test/credo/check/consistency/unused_variable_names_test.exs
+++ test/credo/check/consistency/unused_variable_names_test.exs
@@ -105,12 +105,72 @@ defmodule Credo.Check.Consistency.UnusedVariableNamesTest do
     end)
   end
 
+  test "it should report a violation for different naming schemes with guards (expects anonymous)" do
+    [
+      """
+      defmodule Credo.SampleOne do
+        defmodule Foo do
+          def bar(_, _, _) do
+          end
+        end
+      end
+      """,
+      """
+      defmodule Credo.SampleTwo do
+        defmodule Foo do
+          def bar(_, x2, _x3) when is_nil(x2) do
+          end
+        end
+      end
+      """
+    ]
+    |> to_source_files
+    |> run_check(@described_check)
+    |> assert_issue(fn issue ->
+      assert "_x3" == issue.trigger
+      assert 3 == issue.line_no
+    end)
+  end
+
   test "it should report a violation for different naming schemes (expects meaningful)" do
     [
       """
       defmodule Credo.SampleOne do
         defmodule Foo do
           def bar(name, _) do
+            case name do
+              "foo" <> _name -> "FOO"
+              "bar" <> _name -> "BAR"
+              _name -> "DEFAULT"
+            end
+          end
+        end
+      end
+      """,
+      """
+      defmodule Credo.SampleTwo do
+        defmodule Foo do
+          def bar(list) do
+            Enum.map(list, fn _item -> 1 end)
+          end
+        end
+      end
+      """
+    ]
+    |> to_source_files
+    |> run_check(@described_check)
+    |> assert_issue(fn issue ->
+      assert "_" == issue.trigger
+      assert 3 == issue.line_no
+    end)
+  end
+
+  test "it should report a violation for different naming schemes with guards (expects meaningful)" do
+    [
+      """
+      defmodule Credo.SampleOne do
+        defmodule Foo do
+          def bar(name, _) when is_binary(name) do
             case name do
               "foo" <> _name -> "FOO"
               "bar" <> _name -> "BAR"
