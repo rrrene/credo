138d2d5452b1f6cc9b945482280c875f814beefa
Vinícius Müller <vinigm.nho@gmail.com>
2023-09-23 02:49:29 -0300

    Add duplicated aliases check to .credo.exs

--- .credo.exs
+++ .credo.exs
@@ -95,6 +95,7 @@
           ## Readability Checks
           #
           {Credo.Check.Readability.AliasOrder, []},
+          {Credo.Check.Readability.DuplicatedAliases, []},
           {Credo.Check.Readability.FunctionNames, []},
           {Credo.Check.Readability.LargeNumbers, []},
           {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 120]},
