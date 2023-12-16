46e9ca6796363abc7f94bfc56db6aae20418d319
Frerich Raabe <frerich.raabe@gmail.com>
2023-12-16 12:47:02 +0100

    Update lib/credo/check/refactor/utc_now_truncate.ex
    Co-authored-by: René Föhring <rf@bamaru.de>

--- lib/credo/check/refactor/utc_now_truncate.ex
+++ lib/credo/check/refactor/utc_now_truncate.ex
@@ -6,15 +6,15 @@ defmodule Credo.Check.Refactor.UtcNowTruncate do
       check: """
       `DateTime.utc_now/1` is more efficient than `DateTime.utc_now/0 |> DateTime.truncate/1`.
 
-      This should be refactored:
+      For example, the code here ...
 
           DateTime.utc_now() |> DateTime.truncate(:second)
+          NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
 
-      to look like this:
+      ... can be refactored to look like this:
 
           DateTime.utc_now(:second)
-
-      Dito for `NaiveDateTime`.
+          NaiveDateTime.utc_now(:second)
 
       The reason for this is not just performance, because no separate function
       call is required, but also brevity of the resulting code.
