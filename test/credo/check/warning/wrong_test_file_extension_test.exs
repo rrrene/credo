460f4a5bf9526fd5f882ff68beb2612dc40623d3
René Föhring <rf@bamaru.de>
2024-01-30 20:58:53 +0100

    Remove warning

--- test/credo/check/warning/wrong_test_file_extension_test.exs
+++ test/credo/check/warning/wrong_test_file_extension_test.exs
@@ -1,8 +1,6 @@
 defmodule Credo.Check.Warning.WrongTestFileExtensionTest do
   use Credo.Test.Case
 
-  @described_check Credo.Check.Warning.WrongTestFileExtension
-
   # we can not test this check, because it uses Credo's `:files`/`:included`
   # param to only run on misnamed files :(
 end
