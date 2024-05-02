90b83e8a3df340b1dd9521195b559a278bc21368
René Föhring <rf@bamaru.de>
2024-05-02 22:05:42 +0200

    Don't emit warnings for parsed code
    At least on newer versions of Elixir

--- lib/credo/code.ex
+++ lib/credo/code.ex
@@ -74,16 +74,22 @@ defmodule Credo.Code do
 
   @doc false
   def ast(source, filename \\ "nofilename") when is_binary(source) do
-    case Code.string_to_quoted(source, line: 1, columns: true, file: filename) do
-      {:ok, value} ->
-        {:ok, value}
-
-      {:error, error} ->
-        {:error, [issue_for(error, filename)]}
+    case string_to_quoted(source, filename) do
+      {:ok, value} -> {:ok, value}
+      {:error, error} -> {:error, [issue_for(error, filename)]}
     end
   rescue
     e in UnicodeConversionError ->
       {:error, [issue_for({1, e.message, nil}, filename)]}
+  end
+
+  defp string_to_quoted(source, filename) do
+    Code.string_to_quoted(source,
+      line: 1,
+      columns: true,
+      file: filename,
+      emit_warnings: false
+    )
   end
 
   defp issue_for({line_no, error_message, _}, filename) do
