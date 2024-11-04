c99499b2ddeb2eb32471dcf0a1797529bde3fdaa
René Föhring <rf@bamaru.de>
2024-11-04 21:14:54 +0100

    Remove unsupported formats from Diff command help text
    Refs #1153
    
    We need a better solution long term.
    For now, we should not advertise format options that are not viable.

--- lib/credo/cli/command/diff/diff_output.ex
+++ lib/credo/cli/command/diff/diff_output.ex
@@ -41,7 +41,7 @@ defmodule Credo.CLI.Command.Diff.DiffOutput do
             --enable-disabled-checks  Re-enable disabled checks that match the given strings
             --files-included          Only include these files (accepts globs, can be used multiple times)
             --files-excluded          Exclude these files (accepts globs, can be used multiple times)
-            --format                  Display the list in a specific format (json,flycheck,sarif,oneline)
+            --format                  Display the list in a specific format (json)
             --from-dir                Diff from the given directory
             --from-git-ref            Diff from the given Git ref
             --from-git-merge-base     Diff from where the current HEAD branched off from the given merge base
