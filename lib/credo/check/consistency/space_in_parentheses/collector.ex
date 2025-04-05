d7fce495d20903b7ee5a2b179132d879ddead975
René Föhring <rf@bamaru.de>
2025-04-05 19:24:26 +0200

    Pass reference instead of calling funi
    ... for every line in every file.

--- lib/credo/check/consistency/space_in_parentheses/collector.ex
+++ lib/credo/check/consistency/space_in_parentheses/collector.ex
@@ -4,13 +4,17 @@ defmodule Credo.Check.Consistency.SpaceInParentheses.Collector do
   use Credo.Check.Consistency.Collector
 
   def collect_matches(source_file, _params) do
+    regexes = all_regexes()
+
     source_file
     |> Credo.Code.clean_charlists_strings_sigils_and_comments("")
     |> Credo.Code.to_lines()
-    |> Enum.reduce(%{}, &spaces/2)
+    |> Enum.reduce(%{}, &spaces(&1, &2, regexes))
   end
 
   def find_locations_not_matching(expected, source_file, allow_empty_enums) do
+    regexes = all_regexes()
+
     actual =
       case expected do
         :with_space when allow_empty_enums == true -> :without_space_allow_empty_enums
@@ -21,18 +25,11 @@ defmodule Credo.Check.Consistency.SpaceInParentheses.Collector do
     source_file
     |> Credo.Code.clean_charlists_strings_sigils_and_comments("")
     |> Credo.Code.to_lines()
-    |> List.foldr([], &locate(actual, &1, &2))
+    |> List.foldr([], &locate(actual, &1, &2, regexes))
   end
 
-  defp regexes(),
-    do: [
-      with_space: ~r/[^\?]([\{\[\(]\s+\S|\S\s+[\)\]\}]])/,
-      without_space: ~r/[^\?]([\{\[\(]\S|\S[\)\]\}])/,
-      without_space_allow_empty_enums: ~r/[^\?](?!\{\}|\[\])([\{\[\(]\S|\S[\)\]\}])/
-    ]
-
-  defp spaces({_line_no, line}, acc) do
-    Enum.reduce(regexes(), acc, fn {kind_of_space, regex}, space_map ->
+  defp spaces({_line_no, line}, acc, regexes) do
+    Enum.reduce(regexes, acc, fn {kind_of_space, regex}, space_map ->
       if Regex.match?(regex, line) do
         Map.update(space_map, kind_of_space, 1, &(&1 + 1))
       else
@@ -41,13 +38,23 @@ defmodule Credo.Check.Consistency.SpaceInParentheses.Collector do
     end)
   end
 
-  defp locate(kind_of_space, {line_no, line}, locations) do
-    case Regex.run(regexes()[kind_of_space], line) do
+  defp locate(kind_of_space, {line_no, line}, locations, regexes) do
+    case Regex.run(regexes[kind_of_space], line) do
       nil ->
         locations
 
       match ->
         [[trigger: Enum.at(match, 1), line_no: line_no] | locations]
     end
+  end
+
+  # moved to private function due to deprecation of regexes
+  # in module attributes in Elixir 1.19
+  defp all_regexes() do
+    [
+      with_space: ~r/[^\?]([\{\[\(]\s+\S|\S\s+[\)\]\}]])/,
+      without_space: ~r/[^\?]([\{\[\(]\S|\S[\)\]\}])/,
+      without_space_allow_empty_enums: ~r/[^\?](?!\{\}|\[\])([\{\[\(]\S|\S[\)\]\}])/
+    ]
   end
 end
