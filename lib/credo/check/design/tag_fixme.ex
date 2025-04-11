1afea38dee5d1ccae9d71b52376e716ef14f14ef
René Föhring <rf@bamaru.de>
2025-04-11 19:30:21 +0200

    Update TagHelper to use ast with comments

--- lib/credo/check/design/tag_fixme.ex
+++ lib/credo/check/design/tag_fixme.ex
@@ -36,15 +36,16 @@ defmodule Credo.Check.Design.TagFIXME do
     include_doc? = Params.get(params, :include_doc, __MODULE__)
 
     source_file
-    |> TagHelper.tags(@tag_name, include_doc?)
+    |> TagHelper.find_tags(@tag_name, include_doc?)
     |> Enum.map(&issue_for(issue_meta, &1))
   end
 
-  defp issue_for(issue_meta, {line_no, _line, trigger}) do
+  defp issue_for(issue_meta, {{line_no, column}, _line, trigger}) do
     format_issue(
       issue_meta,
       message: "Found a #{@tag_name} tag in a comment: #{trigger}",
       line_no: line_no,
+      column: column,
       trigger: trigger
     )
   end
