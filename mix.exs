7acc3aba19d8b256516ea7591e1b6bde4daca2a9
René Föhring <rf@bamaru.de>
2025-04-17 05:24:30 +0200

    Add :test_ignore_filters for Elixir 1.19 compat

--- mix.exs
+++ mix.exs
@@ -18,7 +18,13 @@ defmodule Credo.Mixfile do
       package: package(),
       source_url: "https://github.com/rrrene/credo",
       docs: docs(),
-      aliases: aliases()
+      aliases: aliases(),
+      test_ignore_filters: [
+        "test/old_credo.exs",
+        "test/regression/run_older_credo_version.exs",
+        ~r[test/credo/check/housekeeping_.+.exs],
+        ~r[/fixtures/]
+      ]
     ] ++ project_cli_entry()
   end
 
