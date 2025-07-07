5556c77db0745d7023afb2644ca36e72a0b5976e
Tomasz Marek Sulima <Eiji7@users.noreply.github.com>
2025-07-07 03:21:35 +0300

    fix failing test on the reference in re_pattern value in Erlang 28.0

--- test/credo/exs_loader_test.exs
+++ test/credo/exs_loader_test.exs
@@ -17,20 +17,28 @@ defmodule Credo.ExsLoaderTest do
       }
     """
 
+    assert {:ok, safe_result} = Credo.ExsLoader.parse(exs_string, "testfile.ex", @exec, true)
+    assert {:ok, unsafe_result} = Credo.ExsLoader.parse(exs_string, "testfile.ex", @exec, false)
+
     expected = %{
       combine: {:hex, :combine, "0.5.2"},
       cowboy: {:hex, :cowboy, "1.0.2"},
       dirs: ["lib", "src", "test"],
       dirs_sigil: ~w(lib src test),
-      dirs_regex: ~r(lib src test),
       checks: [
         {Style.MaxLineLength, max_length: 100},
         {Style.TrailingBlankLine}
       ]
     }
 
-    assert {:ok, expected} == Credo.ExsLoader.parse(exs_string, "testfile.ex", @exec, true)
-    assert {:ok, expected} == Credo.ExsLoader.parse(exs_string, "testfile.ex", @exec, false)
+    assert expected == Map.delete(safe_result, :dirs_regex)
+    assert expected == Map.delete(unsafe_result, :dirs_regex)
+
+    # Erlang 28.0
+    # match on source field instead of the whole regex since
+    # re_pattern contains reference for each regular expression
+    assert "lib src test" == safe_result.dirs_regex.source
+    assert "lib src test" == unsafe_result.dirs_regex.source
   end
 
   test "Credo.Execution.parse_exs should return error tuple" do
