6f6c687112071eef3761d124c54b15088b730672
René Föhring <rf@bamaru.de>
2025-10-16 21:34:52 +0200

    Update regression testing

--- test/old_credo.exs
+++ test/old_credo.exs
@@ -1,3 +1,4 @@
-Mix.install([{:credo, "~> 1.7"}])
+# Mix.install([{:credo, "~> 1.7"}])
+Mix.install([{:credo, github: "rrrene/credo", ref: "v1.7.12"}])
 
 Credo.run(~w"--mute-exit-status --strict --enable-disabled-checks .+ --no-color --format oneline")
