d61be44787209832b54797b52f3302ebaff909e7
René Föhring <rf@bamaru.de>
2025-10-16 21:37:23 +0200

    Fix regression

--- test/old_credo.exs
+++ test/old_credo.exs
@@ -1,4 +1,4 @@
 # Mix.install([{:credo, "~> 1.7"}])
-Mix.install([{:credo, github: "rrrene/credo", ref: "v1.7.12"}])
+Mix.install([{:credo, github: "rrrene/credo", ref: "v1.7.13"}])
 
 Credo.run(~w"--mute-exit-status --strict --enable-disabled-checks .+ --no-color --format oneline")
