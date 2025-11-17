cef51ea8b5c8e0586b51e5da02c51e94d49f2362
René Föhring <rf@bamaru.de>
2025-11-17 21:55:50 +0100

    Refactor ExpensiveEmptyEnumCheck

--- lib/credo/check/warning/expensive_empty_enum_check.ex
+++ lib/credo/check/warning/expensive_empty_enum_check.ex
@@ -5,8 +5,8 @@ defmodule Credo.Check.Warning.ExpensiveEmptyEnumCheck do
     explanations: [
       # TODO: improve checkdoc
       check: """
-      Checking if the size of the enum is `0` can be very expensive, since you are
-      determining the exact count of elements.
+      Checking if the size of the enum is `0` (or not `0`) can be very expensive,
+      since you are determining the exact count of elements.
 
       Checking if an enum is empty should be done by using
 
@@ -17,7 +17,7 @@ defmodule Credo.Check.Warning.ExpensiveEmptyEnumCheck do
           list == []
 
 
-      For Enum.count/2: Checking if an enum doesn't contain specific elements should
+      For `Enum.count/2`: Checking if an enum doesn't contain specific elements should
       be done by using
 
           not Enum.any?(enum, condition)
@@ -28,9 +28,9 @@ defmodule Credo.Check.Warning.ExpensiveEmptyEnumCheck do
   @doc false
   @impl true
   def run(%SourceFile{} = source_file, params) do
-    issue_meta = IssueMeta.for(source_file, params)
-
-    Credo.Code.prewalk(source_file, &traverse(&1, &2, issue_meta))
+    ctx = Context.build(source_file, params, __MODULE__)
+    result = Credo.Code.prewalk(source_file, &walk/2, ctx)
+    result.issues
   end
 
   @enum_count_pattern quote do: {
@@ -43,74 +43,44 @@ defmodule Credo.Check.Warning.ExpensiveEmptyEnumCheck do
     {@enum_count_pattern, "Enum.count"},
     {@length_pattern, "length"}
   ]
-  @eq_operators [:==, :!=, :===, :!==, :>, :<]
+  @operators [:==, :!=, :===, :!==, :>, :<]
 
-  # Comparisons against 0
-  for {pattern, trigger} <- @comparisons,
-      operator <- @eq_operators do
-    defp traverse(
-           {unquote(operator), _meta, [unquote(pattern), 0]} = ast,
-           issues,
-           issue_meta
-         ) do
-      {ast, issues_for_call(unquote(trigger), issues, issue_meta, ast)}
-    end
-
-    defp traverse(
-           {unquote(operator), _meta, [0, unquote(pattern)]} = ast,
-           issues,
-           issue_meta
-         ) do
-      {ast, issues_for_call(unquote(trigger), issues, issue_meta, ast)}
-    end
-  end
-
-  # Comparisons against 1
   for {pattern, trigger} <- @comparisons do
-    defp traverse(
-           {:>=, _meta, [unquote(pattern), 1]} = ast,
-           issues,
-           issue_meta
-         ) do
-      {ast, issues_for_call(unquote(trigger), issues, issue_meta, ast)}
+    # Comparisons against 0
+    defp walk({op, meta, [unquote(pattern), 0]} = ast, ctx) when op in @operators do
+      {ast, put_issue(ctx, issue_for(ctx, meta, unquote(trigger), suggest(ast)))}
     end
 
-    defp traverse(
-           {:<=, _meta, [1, unquote(pattern)]} = ast,
-           issues,
-           issue_meta
-         ) do
-      {ast, issues_for_call(unquote(trigger), issues, issue_meta, ast)}
+    defp walk({op, meta, [0, unquote(pattern)]} = ast, ctx) when op in @operators do
+      {ast, put_issue(ctx, issue_for(ctx, meta, unquote(trigger), suggest(ast)))}
+    end
+
+    # Comparisons against 1
+    defp walk({:>=, meta, [unquote(pattern), 1]} = ast, ctx) do
+      {ast, put_issue(ctx, issue_for(ctx, meta, unquote(trigger), suggest(ast)))}
+    end
+
+    defp walk({:<=, meta, [1, unquote(pattern)]} = ast, ctx) do
+      {ast, put_issue(ctx, issue_for(ctx, meta, unquote(trigger), suggest(ast)))}
     end
   end
 
-  defp traverse(ast, issues, _issue_meta) do
-    {ast, issues}
-  end
-
-  defp issues_for_call(trigger, issues, issue_meta, ast) do
-    meta = get_meta(ast)
-    [issue_for(issue_meta, meta, trigger, suggest(ast)) | issues]
+  defp walk(ast, ctx) do
+    {ast, ctx}
   end
 
   defp suggest({_op, _, [_, {_pattern, _, args}]}), do: suggest_for_arity(Enum.count(args))
   defp suggest({_op, _, [{_pattern, _, args}, _]}), do: suggest_for_arity(Enum.count(args))
 
-  defp get_meta({_op, _, [_, {{:., _, [{:__aliases__, meta, _}, _]}, _, _}]}), do: meta
-  defp get_meta({_op, _, [_, {_, meta, _}]}), do: meta
-  defp get_meta({_op, _, [{{:., _, [{:__aliases__, meta, _}, _]}, _, _}, _]}), do: meta
-  defp get_meta({_op, _, [{_, meta, _}, _]}), do: meta
-
   defp suggest_for_arity(2), do: "`not Enum.any?/2`"
-  defp suggest_for_arity(1), do: "`Enum.empty?/1` or `list == []`"
+  defp suggest_for_arity(1), do: "`Enum.empty?/1`"
 
-  defp issue_for(issue_meta, meta, trigger, suggestion) do
+  defp issue_for(ctx, meta, trigger, suggestion) do
     format_issue(
-      issue_meta,
-      message: "#{trigger} is expensive, prefer #{suggestion}.",
+      ctx,
+      message: "Using `#{trigger}/1` is expensive, prefer #{suggestion}.",
       trigger: trigger,
-      line_no: meta[:line],
-      column: meta[:column]
+      line_no: meta[:line]
     )
   end
 end
