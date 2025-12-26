7109c9d9fcab5b12a8446eb82b2bfc7771f034f1
René Föhring <rf@bamaru.de>
2025-12-26 06:34:10 +0100

    Add test for #1235

--- test/credo/check/readability/trailing_white_space_test.exs
+++ test/credo/check/readability/trailing_white_space_test.exs
@@ -351,6 +351,30 @@ defmodule Credo.Check.Readability.TrailingWhiteSpaceTest do
     |> refute_issues()
   end
 
+  test "it should NOT report line endings in docs" do
+    ~S'''
+    defmodule Mix.Tasks.Publish do
+      @moduledoc """
+      Publishes the package to hex.pm with pre-flight checks and version management.
+
+      ## Usage
+
+          mix publish             # Full publish workflow
+          mix publish --dry-run   # Run checks and build, but don't publish
+          mix publish --skip-checks # Skip pre-flight checks
+          mix publish --revert    # Revert current version from hex.pm
+          mix publish --republish # Revert and republish in one step
+
+      ## Pre-flight Checks
+      ...
+      """
+    end
+    '''
+    |> to_source_file
+    |> run_check(@described_check)
+    |> refute_issues()
+  end
+
   test "it should NOT report escaped heredocs in regular heredocs" do
     ~S'''
     defmodule Example do
