82130ca97734de211a387fb7239597307a3303ed
René Föhring <rf@bamaru.de>
2025-12-26 06:37:36 +0100

    Fix last commit

--- test/credo/check/readability/trailing_white_space_test.exs
+++ test/credo/check/readability/trailing_white_space_test.exs
@@ -356,20 +356,21 @@ defmodule Credo.Check.Readability.TrailingWhiteSpaceTest do
     defmodule Mix.Tasks.Publish do
       @moduledoc """
       Publishes the package to hex.pm with pre-flight checks and version management.
-
+      $$$
       ## Usage
-
+      $$$
           mix publish             # Full publish workflow
           mix publish --dry-run   # Run checks and build, but don't publish
           mix publish --skip-checks # Skip pre-flight checks
           mix publish --revert    # Revert current version from hex.pm
           mix publish --republish # Revert and republish in one step
-
+      $$$
       ## Pre-flight Checks
       ...
       """
     end
     '''
+    |> String.replace("$$$", "")
     |> to_source_file
     |> run_check(@described_check)
     |> refute_issues()
