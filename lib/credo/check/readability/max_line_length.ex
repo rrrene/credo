4bde67f524c94931b96a1fe68c12f138063d55e7
René Föhring <rf@bamaru.de>
2026-03-11 08:13:30 +0100

    Fix MaxLineLength for any value

--- lib/credo/check/readability/max_line_length.ex
+++ lib/credo/check/readability/max_line_length.ex
@@ -150,9 +150,8 @@ defmodule Credo.Check.Readability.MaxLineLength do
   defp contains_url?([]), do: false
 
   defp contains_url?({_, _, [_ | _] = contents, _}), do: Enum.any?(contents, &contains_url?/1)
-  defp contains_url?({_, _, _, _}), do: false
 
-  defp contains_url?(value) when is_atom(value), do: false
+  defp contains_url?(_), do: false
 
   defp issue_for(issue_meta, line_no, line_length, max_length) do
     column = max_length + 1
