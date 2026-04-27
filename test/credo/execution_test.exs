rrrene/credo

commit 2e53ab5a92bc222c2bc0382a4c99d5cffac66e04

Reduce %Execution{} struct fields to 28

lib/credo/execution.ex
@@ -7,37 +7,21 @@ defmodule Credo.Execution do
   @doc """
   The `Credo.Execution` struct is created and manipulated via the `Credo.Execution` module.
   """
-  defstruct argv: [],
-            cli_options: nil,
-            # TODO: these initial switches should also be %Credo.CLI.Switch{} struct
-            cli_switches: [
-              debug: :boolean,
-              color: :boolean,
-              config_name: :string,
-              config_file: :string,
-              working_dir: :string
-            ],
-            cli_aliases: [C: :config_name, D: :debug],
-            cli_switch_plugin_param_converters: [],
+  defstruct cli_options: nil,
 
-            # config
-            files: nil,
-            color: true,
-            debug: false,
-            checks: nil,
+            # TODO: 1.8 - maybe move these to new `config` field
             requires: [],
             plugins: [],
             parse_timeout: 5000,
+            checks: nil,
+            files: nil,
+            debug: false,
             strict: false,
-
-            # options, set by the command line
+            color: true,
             format: nil,
             help: false,
             verbose: false,
             version: false,
-
-            # options, that are kept here for legacy reasons
-            all: false,
             crash_on_error: true,
             enable_disabled_checks: nil,
             ignore_checks_tags: [],
@@ -48,25 +32,38 @@ defmodule Credo.Execution do
             only_checks: nil,
             read_from_stdin: false,
 
-            # This is no longer used, but we keep it so existing plugins that use it don't break
-            max_concurrent_check_runs: nil,
-
             # state, which is accessed and changed over the course of Credo's execution
-            pipeline_map: %{},
-            commands: %{},
-            config_files: [],
-            current_task: nil,
-            parent_task: nil,
-            initializing_plugin: nil,
             halted: false,
-            config_files_pid: nil,
-            source_files_pid: nil,
-            issues_pid: nil,
-            timing_pid: nil,
-            skipped_checks: nil,
             assigns: %{},
             results: %{},
-            config_comment_map: %{}
+            current_task: nil,
+            parent_task: nil,
+            private: nil
+
+  defmodule Private do
+    @moduledoc false
+
+    @doc false
+    defstruct commands: %{},
+              config_comment_map: %{},
+              pipeline_map: %{},
+              initializing_plugin: nil,
+              cli_switches: [
+                debug: :boolean,
+                color: :boolean,
+                config_name: :string,
+                config_file: :string,
+                working_dir: :string
+              ],
+              cli_aliases: [C: :config_name, D: :debug],
+              cli_switch_plugin_param_converters: [],
+              skipped_checks: nil,
+              config_files_pid: nil,
+              source_files_pid: nil,
+              issues_pid: nil,
+              timing_pid: nil,
+              max_concurrent_check_runs: nil
+  end
 
   @typedoc false
   @type t :: %__MODULE__{}
@@ -119,11 +116,16 @@ defmodule Credo.Execution do
   alias Credo.Execution.ExecutionSourceFiles
   alias Credo.Execution.ExecutionTiming
 
+  alias Credo.CLI.Options
+
   @doc "Builds an Execution struct for the given `argv`."
   def build(argv \\ []) when is_list(argv) do
     max_concurrent_check_runs = System.schedulers_online()
 
-    %__MODULE__{argv: argv, max_concurrent_check_runs: max_concurrent_check_runs}
+    %__MODULE__{
+      cli_options: %Options{argv: argv},
+      private: %__MODULE__.Private{max_concurrent_check_runs: max_concurrent_check_runs}
+    }
     |> put_pipeline(@execution_pipeline_key, @execution_pipeline)
     |> put_builtin_command("categories", Credo.CLI.Command.Categories.CategoriesCommand)
     |> put_builtin_command("diff", Credo.CLI.Command.Diff.DiffCommand)
@@ -140,7 +142,7 @@ defmodule Credo.Execution do
 
   @doc false
   def build(%__MODULE__{} = previous_exec, files_that_changed) when is_list(files_that_changed) do
-    previous_exec.argv
+    previous_exec.cli_options.argv
     |> build()
     |> put_rerun(previous_exec, files_that_changed)
   end
@@ -156,6 +158,16 @@ defmodule Credo.Execution do
     |> ExecutionIssues.start_server()
     |> ExecutionSourceFiles.start_server()
     |> ExecutionTiming.start_server()
+  end
+
+  @doc false
+  def put_private(exec, private_field, value) do
+    Map.put(exec, :private, Map.put(exec.private, private_field, value))
+  end
+
+  @doc false
+  def get_private(exec, private_field) do
+    Map.get(exec.private, private_field)
   end
 
   @doc """
@@ -267,13 +279,19 @@ defmodule Credo.Execution do
     end)
   end
 
+  @all_min_priority -99
+
+  def show_all?(%__MODULE__{} = exec) do
+    exec.min_priority <= @all_min_priority
+  end
+
   @doc """
   Sets the exec values which `strict` implies (if applicable).
   """
   def set_strict(exec)
 
   def set_strict(%__MODULE__{strict: true} = exec) do
-    %{exec | all: true, min_priority: -99}
+    %{exec | min_priority: @all_min_priority}
   end
 
   def set_strict(%__MODULE__{strict: false} = exec) do
@@ -312,8 +330,8 @@ defmodule Credo.Execution do
       # => ["categories", "diff", "explain", "gen.check", "gen.config", "help", "info",
       #     "list", "suggest", "version"]
   """
-  def get_valid_command_names(%__MODULE__{} = exec) do
-    Map.keys(exec.commands)
+  def get_valid_command_names(exec) do
+    exec |> get_private(:commands) |> Map.keys()
   end
 
   @doc """
@@ -322,33 +340,40 @@ defmodule Credo.Execution do
       Credo.Execution.get_command(exec, "explain")
       # => Credo.CLI.Command.Explain.ExplainCommand
   """
-  def get_command(%__MODULE__{} = exec, name) do
-    Map.get(exec.commands, name) ||
+  def get_command(exec, name) do
+    commands = get_private(exec, :commands)
+
+    Map.get(commands, name) ||
       raise """
       Command not found: "#{inspect(name)}"
 
-      Registered commands: #{inspect(exec.commands, pretty: true)}
+      Registered commands: #{inspect(commands, pretty: true)}
       """
   end
 
   @doc false
-  def put_command(%__MODULE__{} = exec, _plugin_mod, name, command_mod) do
-    commands = Map.put(exec.commands, name, command_mod)
+  def put_command(exec, _plugin_mod, name, command_mod) do
+    commands = get_private(exec, :commands)
+    commands = Map.put(commands, name, command_mod)
 
-    %{exec | commands: commands}
+    exec
+    |> put_private(:commands, commands)
     |> command_mod.init()
   end
 
   @doc false
-  def set_initializing_plugin(%__MODULE__{initializing_plugin: nil} = exec, plugin_mod) do
-    %{exec | initializing_plugin: plugin_mod}
+  def set_initializing_plugin(
+        %__MODULE__{private: %{initializing_plugin: nil}} = exec,
+        plugin_mod
+      ) do
+    put_private(exec, :initializing_plugin, plugin_mod)
   end
 
-  def set_initializing_plugin(%__MODULE__{} = exec, nil) do
-    %{exec | initializing_plugin: nil}
+  def set_initializing_plugin(exec, nil) do
+    put_private(exec, :initializing_plugin, nil)
   end
 
-  def set_initializing_plugin(%__MODULE__{initializing_plugin: mod1}, mod2) do
+  def set_initializing_plugin(%__MODULE__{private: %{initializing_plugin: mod1}}, mod2) do
     raise "Attempting to initialize plugin #{inspect(mod2)}, " <>
             "while already initializing plugin #{mod1}"
   end
@@ -395,15 +420,17 @@ defmodule Credo.Execution do
   end
 
   @doc false
-  def put_cli_switch(%__MODULE__{} = exec, _plugin_mod, name, type) do
-    %{exec | cli_switches: exec.cli_switches ++ [{name, type}]}
+  def put_cli_switch(%__MODULE__{private: %{cli_switches: cli_switches}} = exec, _plugin_mod, name, type) do
+    put_private(exec, :cli_switches, cli_switches ++ [{name, type}])
   end
 
   @doc false
   def put_cli_switch_alias(%__MODULE__{} = exec, _plugin_mod, _name, nil), do: exec
 
-  def put_cli_switch_alias(%__MODULE__{} = exec, _plugin_mod, name, alias_name) do
-    %{exec | cli_aliases: exec.cli_aliases ++ [{alias_name, name}]}
+  def put_cli_switch_alias(exec, _plugin_mod, name, alias_name) do
+    cli_aliases = get_private(exec, :cli_aliases) ++ [{alias_name, name}]
+
+    put_private(exec, :cli_aliases, cli_aliases)
   end
 
   @doc false
@@ -415,11 +442,10 @@ defmodule Credo.Execution do
       ) do
     converter_tuple = {cli_switch_name, plugin_mod, plugin_param_name}
 
-    %{
-      exec
-      | cli_switch_plugin_param_converters:
-          exec.cli_switch_plugin_param_converters ++ [converter_tuple]
-    }
+    cli_switch_plugin_param_converters =
+      get_private(exec, :cli_switch_plugin_param_converters) ++ [converter_tuple]
+
+    put_private(exec, :cli_switch_plugin_param_converters, cli_switch_plugin_param_converters)
   end
 
   # Assigns
@@ -660,16 +686,20 @@ defmodule Credo.Execution do
   # Pipelines
 
   @doc false
-  defp get_pipeline(%__MODULE__{} = exec, pipeline_key) do
-    case exec.pipeline_map[get_pipeline_key(exec, pipeline_key)] do
+  defp get_pipeline(exec, pipeline_key) do
+    pipeline_map = get_private(exec, :pipeline_map)
+
+    case pipeline_map[get_pipeline_key(exec, pipeline_key)] do
       nil -> raise "Could not find execution pipeline for '#{pipeline_key}'"
       pipeline -> pipeline
     end
   end
 
   @doc false
-  defp get_pipeline_key(%__MODULE__{} = exec, pipeline_key) do
-    case exec.pipeline_map[pipeline_key] do
+  defp get_pipeline_key(exec, pipeline_key) do
+    pipeline_map = get_private(exec, :pipeline_map)
+
+    case pipeline_map[pipeline_key] do
       nil -> @execution_pipeline_key_backwards_compatibility_map[pipeline_key]
       _ -> pipeline_key
     end
@@ -695,10 +725,11 @@ defmodule Credo.Execution do
         print_results: [ {MyProject.PrintResults, []} ]
       )
   """
-  def put_pipeline(%__MODULE__{} = exec, pipeline_key, pipeline) do
-    new_pipelines = Map.put(exec.pipeline_map, pipeline_key, pipeline)
+  def put_pipeline(exec, pipeline_key, pipeline) do
+    pipeline_map = get_private(exec, :pipeline_map)
+    pipeline_map = Map.put(pipeline_map, pipeline_key, pipeline)
 
-    %{exec | pipeline_map: new_pipelines}
+    put_private(exec, :pipeline_map, pipeline_map)
   end
 
   @doc """

test/credo/execution_test.exs
@@ -3,16 +3,20 @@ defmodule Credo.ExecutionTest do
 
   alias Credo.Execution
 
-  test "it should work for put_assign & get_assign" do
-    exec = Execution.put_assign(%Execution{}, "foo", "bar")
+  setup do
+    [exec: %Execution{private: %Execution.Private{}}]
+  end
+
+  test "it should work for put_assign & get_assign", %{exec: exec} do
+    exec = Execution.put_assign(exec, "foo", "bar")
 
     assert Execution.get_assign(exec, "foo") == "bar"
   end
 
-  test "it should work for put_assign_in /1" do
+  test "it should work for put_assign_in /1", %{exec: exec} do
     exec =
       Execution.put_assign(
-        %Execution{},
+        exec,
         ["credo.magic_funs", :foo],
         "bar"
       )
@@ -22,10 +26,10 @@ defmodule Credo.ExecutionTest do
     assert Execution.get_assign(exec, ["credo.magic_funs", :none_existing], :baz) == :baz
   end
 
-  test "it should work for put_assign_in and get_assign /2" do
+  test "it should work for put_assign_in and get_assign /2", %{exec: exec} do
     exec =
       Execution.put_assign(
-        %Execution{},
+        exec,
         ["credo.magic_funs", Credo.Check.Readability.ModuleDoc, "foo"],
         "bar"
       )
@@ -40,9 +44,9 @@ defmodule Credo.ExecutionTest do
            ]) == "bar"
   end
 
-  test "it should work for append_task/4" do
-    exec = %Execution{
-      pipeline_map: %{
+  test "it should work for append_task/4", %{exec: exec} do
+    exec =
+      Execution.put_private(exec, :pipeline_map, %{
         Execution => [
           parse_cli_options: [
             {Credo.Execution.Task.ParseOptions, []}
@@ -51,8 +55,7 @@ defmodule Credo.ExecutionTest do
             {Credo.Execution.Task.ValidateOptions, []}
           ]
         ]
-      }
-    }
+      })
 
     expected_pipeline_map = %{
       Execution => [
@@ -68,12 +71,12 @@ defmodule Credo.ExecutionTest do
 
     result = Execution.append_task(exec, Credo, nil, :validate_cli_options, Credo.ExecutionTest)
 
-    assert expected_pipeline_map == result.pipeline_map
+    assert expected_pipeline_map == Execution.get_private(result, :pipeline_map)
   end
 
-  test "it should work for prepend_task/4" do
-    exec = %Execution{
-      pipeline_map: %{
+  test "it should work for prepend_task/4", %{exec: exec} do
+    exec =
+      Execution.put_private(exec, :pipeline_map, %{
         Execution => [
           parse_cli_options: [
             {Credo.Execution.Task.ParseOptions, []}
@@ -82,8 +85,7 @@ defmodule Credo.ExecutionTest do
             {Credo.Execution.Task.ValidateOptions, []}
           ]
         ]
-      }
-    }
+      })
 
     expected_pipeline_map = %{
       Execution => [
@@ -99,14 +101,16 @@ defmodule Credo.ExecutionTest do
 
     result = Execution.prepend_task(exec, Credo, nil, :validate_cli_options, Credo.ExecutionTest)
 
-    assert expected_pipeline_map == result.pipeline_map
+    assert expected_pipeline_map == Execution.get_private(result, :pipeline_map)
   end
 
-  test "it should work for append_task/5 for Credo.CLI.Command.Suggest.SuggestCommand" do
+  test "it should work for append_task/5 for Credo.CLI.Command.Suggest.SuggestCommand", %{
+    exec: exec
+  } do
     pipeline_key = Credo.CLI.Command.Suggest.SuggestCommand
 
-    exec = %Execution{
-      pipeline_map: %{
+    exec =
+      Execution.put_private(exec, :pipeline_map, %{
         Credo.CLI.Command.Suggest.SuggestCommand => [
           parse_cli_options: [
             {Credo.Execution.Task.ParseOptions, []}
@@ -115,8 +119,7 @@ defmodule Credo.ExecutionTest do
             {Credo.Execution.Task.ValidateOptions, []}
           ]
         ]
-      }
-    }
+      })
 
     expected_pipeline_map = %{
       Credo.CLI.Command.Suggest.SuggestCommand => [
@@ -133,14 +136,16 @@ defmodule Credo.ExecutionTest do
     result =
       Execution.append_task(exec, Credo, pipeline_key, :validate_cli_options, Credo.ExecutionTest)
 
-    assert expected_pipeline_map == result.pipeline_map
+    assert expected_pipeline_map == Execution.get_private(result, :pipeline_map)
   end
 
-  test "it should work for prepend_task/5 for Credo.CLI.Command.Suggest.SuggestCommand" do
+  test "it should work for prepend_task/5 for Credo.CLI.Command.Suggest.SuggestCommand", %{
+    exec: exec
+  } do
     pipeline_key = Credo.CLI.Command.Suggest.SuggestCommand
 
-    exec = %Execution{
-      pipeline_map: %{
+    exec =
+      Execution.put_private(exec, :pipeline_map, %{
         Credo.CLI.Command.Suggest.SuggestCommand => [
           parse_cli_options: [
             {Credo.Execution.Task.ParseOptions, []}
@@ -149,8 +154,7 @@ defmodule Credo.ExecutionTest do
             {Credo.Execution.Task.ValidateOptions, []}
           ]
         ]
-      }
-    }
+      })
 
     expected_pipeline_map = %{
       Credo.CLI.Command.Suggest.SuggestCommand => [
@@ -173,14 +177,14 @@ defmodule Credo.ExecutionTest do
         Credo.ExecutionTest
       )
 
-    assert expected_pipeline_map == result.pipeline_map
+    assert expected_pipeline_map == Execution.get_private(result, :pipeline_map)
   end
 
-  test "it should work for append_task/5 for suggest when using old syntax" do
+  test "it should work for append_task/5 for suggest when using old syntax", %{exec: exec} do
     pipeline_key = Credo.CLI.Command.Suggest.SuggestCommand
 
-    exec = %Execution{
-      pipeline_map: %{
+    exec =
+      Execution.put_private(exec, :pipeline_map, %{
         "suggest" => [
           parse_cli_options: [
             {Credo.Execution.Task.ParseOptions, []}
@@ -189,8 +193,7 @@ defmodule Credo.ExecutionTest do
             {Credo.Execution.Task.ValidateOptions, []}
           ]
         ]
-      }
-    }
+      })
 
     expected_pipeline_map = %{
       "suggest" => [
@@ -207,14 +210,14 @@ defmodule Credo.ExecutionTest do
     result =
       Execution.append_task(exec, Credo, pipeline_key, :validate_cli_options, Credo.ExecutionTest)
 
-    assert expected_pipeline_map == result.pipeline_map
+    assert expected_pipeline_map == Execution.get_private(result, :pipeline_map)
   end
 
-  test "it should work for prepend_task/5 for suggest when using old syntax" do
+  test "it should work for prepend_task/5 for suggest when using old syntax", %{exec: exec} do
     pipeline_key = Credo.CLI.Command.Suggest.SuggestCommand
 
-    exec = %Execution{
-      pipeline_map: %{
+    exec =
+      Execution.put_private(exec, :pipeline_map, %{
         "suggest" => [
           parse_cli_options: [
             {Credo.Execution.Task.ParseOptions, []}
@@ -223,8 +226,7 @@ defmodule Credo.ExecutionTest do
             {Credo.Execution.Task.ValidateOptions, []}
           ]
         ]
-      }
-    }
+      })
 
     expected_pipeline_map = %{
       "suggest" => [
@@ -247,6 +249,6 @@ defmodule Credo.ExecutionTest do
         Credo.ExecutionTest
       )
 
-    assert expected_pipeline_map == result.pipeline_map
+    assert expected_pipeline_map == Execution.get_private(result, :pipeline_map)
   end
 end

.formatter.exs
@@ -9,5 +9,6 @@ export_locals_without_parens = [
 [
   inputs: ["{mix,.formatter,.credo}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: export_locals_without_parens,
-  export: [locals_without_parens: export_locals_without_parens]
+  export: [locals_without_parens: export_locals_without_parens],
+  line_length: 120
 ]

lib/credo/check.ex
@@ -171,6 +171,7 @@ defmodule Credo.Check do
     warning: 16
   }
 
+  alias Credo.Execution
   alias Credo.Service.SourceFileScopePriorities
   alias Credo.Check
   alias Credo.Check.Params
@@ -411,7 +412,7 @@ defmodule Credo.Check do
       defp do_run_on_all_source_files(exec, source_files, params) do
         source_files
         |> Task.async_stream(fn source -> run_on_source_file(exec, source, params) end,
-          max_concurrency: exec.max_concurrent_check_runs,
+          max_concurrency: Execution.get_private(exec, :max_concurrent_check_runs),
           timeout: :infinity,
           ordered: false
         )

lib/credo/cli/command.ex
@@ -167,5 +167,5 @@ defmodule Credo.CLI.Command do
 
   @callback treat_unknown_args_as_files?() :: boolean()
 
-  @callback cli_switches() :: [Map.t()]
+  @callback cli_switches() :: [map()]
 end

lib/credo/cli/command/diff/output/default.ex
@@ -176,8 +176,13 @@ defmodule Credo.CLI.Command.Diff.Output.Default do
     |> do_print_issues(source_file_map, exec, term_width)
   end
 
-  defp per_category(%Execution{all: true}), do: 1_000_000
-  defp per_category(%Execution{all: false}), do: @per_category
+  defp per_category(%Execution{} = exec) do
+    if Execution.show_all?(exec) do
+      1_000_000
+    else
+      @per_category
+    end
+  end
 
   defp do_print_issues(
          issues,

lib/credo/cli/command/diff/task/get_git_diff.ex
@@ -103,7 +103,7 @@ defmodule Credo.CLI.Command.Diff.Task.GetGitDiff do
 
   defp run_credo_on_dir(exec, dirname, previous_git_ref, given_ref) do
     {previous_argv, _last_arg} =
-      exec.argv
+      exec.cli_options.argv
       |> Enum.slice(1..-1//1)
       |> Enum.reduce({[], nil}, fn
         _, {argv, "--working-dir"} -> {Enum.slice(argv, 1..-2//1), nil}

lib/credo/cli/command/list/list_command.ex
@@ -1,6 +1,4 @@
 defmodule Credo.CLI.Command.List.ListCommand do
-  @moduledoc false
-
   use Credo.CLI.Command,
     short_description: "List all issues grouped by files",
     treat_unknown_args_as_files: true,

commit 3bcddbae303138dabed16173832e5b7e3ccd307f

Fix `--all` option
Commit 2e53ab5a broke the `--all` command line flag, causing the truncation message, like:

```
┃  ...  (17 more, use `--all` to show them)
```

to be shown even when `--all` was supplied. With this change, `--all` is restored to listing all issues.

lib/credo/execution.ex
@@ -307,6 +307,8 @@ defmodule Credo.Execution do
 
   @all_min_priority -99
 
+  def show_all?(%__MODULE__{cli_options: %{switches: %{all: true}}}), do: true
+
   def show_all?(%__MODULE__{} = exec) do
     exec.config.min_priority <= @all_min_priority
   end

test/credo/execution_test.exs
@@ -1,10 +1,16 @@
 defmodule Credo.ExecutionTest do
   use ExUnit.Case
 
+  alias Credo.CLI.Options
   alias Credo.Execution
 
   setup do
     [exec: %Execution{config: %Execution.RuntimeConfig{}, private: %Execution.Private{}}]
+  end
+
+  test "show_all? should be true when --all was given" do
+    exec = %Execution{cli_options: %Options{switches: %{all: true}}}
+    assert Execution.show_all?(exec)
   end
 
   test "it should work for put_assign & get_assign", %{exec: exec} do